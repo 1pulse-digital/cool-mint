30e6362f94e52b6ca4682e48b91d6499ac39a7b0
Parson <parson@1pulse.co.za>
2024-01-23 12:12:12 +0200

    about-component

---
+++ app/page.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+//import Hero from "@/components/hero";
+//import Navbar from "@/components/navbar";
+import { AboutUs } from "@/components/aboutUs";
+//import { Offerings } from "@/components/offerings";
+
+export const Home = () => {
+  return (
+  <div>
+ {/* <Navbar /> */}
+  {/* <Hero /> */}
+  <AboutUs />
+  {/* <Offerings /> */}
+  </div>
+  )
+}
+export default Home;
