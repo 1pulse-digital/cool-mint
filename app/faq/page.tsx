8cc7eae9f5cf4649960b2dd9c0ab1da0af5bcf42
Parson <parson@1pulse.co.za>
2024-02-07 19:15:42 +0200

    feat: ✨ faq page

---
+++ app/faq/page.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export const FAQ = () => {
+    return(
+        <div className={"grid grid-cols-1 bg-background py-12 px-8 "}>
+        <div className={"grid content-center font-karla p-5 sm:p-10 md:max-w-prose"}>
+          <h1 className={"text-headings text-secondary font-helvetica font-bold py-4 text-center"}>FAQ</h1>
+          <p className={"text-BodyText text-textColor text-center font-helvetica font-light"}>
+          Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat.
+          </p>
+        </div>
+     </div>
+    )
+}
+
+export default FAQ;
