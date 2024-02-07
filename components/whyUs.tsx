32b7c1e4af5fe5b89151eef7e7c19c0e49b685a4
Parson <parson@1pulse.co.za>
2024-02-07 19:04:43 +0200

    feat: ✨ why us component

--- components/whyUs.tsx
+++ components/whyUs.tsx
@@ -6,7 +6,7 @@ import Button from "./base/button";
 export const WhyUs = () => {
   return(
       <div className={"grid grid-cols-1 bg-background py-12 px-8 "}>
-        <div className={"grid content-center font-karla p-5 sm:p-10 md:max-w-prose"}>
+        <div className={"grid content-center font-helvetica p-5 sm:p-10 md:max-w-prose"}>
           <h1 className={"text-headings text-secondary font-helvetica font-bold py-4 text-center"}>Why us<span className="text-primary">?</span></h1>
           <p className={"text-BodyText text-textColor text-center font-helvetica font-light"}>
           Made In Workshop (MIW) is a shared workshop and fabrication studio. Not just a workshop with a
