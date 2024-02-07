df458904a94346302c1f70add99441e8bfe119de
Erick Van Der Linde <erick@meshtrade.co>
2024-02-07 18:31:38 +0200

    refactor: cleanup and format with prettier

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,33 +1,26 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import './globals.css'
-import 'tailwindcss/tailwind.css';
-import 'tailwindcss-debug-screens';
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
 
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 const isDev = process?.env.NODE_ENV === "development";
 
 export const metadata: Metadata = {
-  title: 'Made in Workshop',
-  description: 'Shared workshop and fabrication studio',
-}
+  title: "Made in Workshop",
+  description: "Shared workshop and fabrication studio"
+};
 
 export default function RootLayout({
-  children,
-}: {
+                                     children
+                                   }: {
   children: React.ReactNode
 }) {
   return (
     <html lang="en">
-    
     <body className={`${isDev ? "debug-screens " : ""}${inter.className}`}>
-        {children}
-      </body>
-     
+    {children}
+    </body>
     </html>
-   
-    
-    
-  )
+  );
 }
