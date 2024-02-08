e15922c40d2d8a86305ee0517063319883949287
Parson <parson@1pulse.co.za>
2024-02-08 16:10:42 +0200

    feat: ✨ updated-workshops page

--- app/workshops/page.tsx
+++ app/workshops/page.tsx
@@ -1,16 +1,11 @@
-import IndustrialMachineTools from "@/components/base/IndustrialMachineTools";
-import Button from "@/components/base/button";
-import { Faqs } from "@/components/base/faqs";
 import { FilterButtons } from "@/components/base/filterButtons";
 import GetInTouch from "@/components/base/getInTouch";
 import UpcomingWorkshops from "@/components/base/upcomingWorkshops";
-import Link from "next/link";
 import React from "react";
 
 const Workshops = () => {
   return(
       <div className={"grid grid-cols-1 bg-background py-12 "}>
-       
         <div className={"grid content-center font-helvetica sm:p-10 md:max-w-prose px-8"}>
           <h1 className={"text-headings text-secondary font-helvetica font-bold py-4 text-center leading-tight"}>Upcoming Workshops</h1>
           <p className={"text-BodyText text-textColor text-center font-helvetica font-light"}>
@@ -21,12 +16,6 @@ const Workshops = () => {
          <FilterButtons />
          </div>
         <UpcomingWorkshops />
-        
-       
-      
-      
-        
-        <Faqs />
         <GetInTouch />
      </div>
   );
