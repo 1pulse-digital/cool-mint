5364c06207412f06f4e5bf7e9d11a0b4e21c5f78
Parson <parson@1pulse.co.za>
2024-02-16 17:08:04 +0200

    feat: ✨ updated community component

--- components/community.tsx
+++ components/community.tsx
@@ -5,28 +5,28 @@ import gallery from "../images/banner.webp";
 export const Community = () => {
     return(
         <div className={"bg-background px-8"}>
-          <div className={"content-center font-karla p-5 sm:p-10 md:max-w-prose"}>
+          <div className={"content-center font-helvetica p-5"}>
             <h1 className={"text-headings text-secondary font-helvetica font-bold py-4 text-center leading-tight"}>Maker Community.</h1>
-            <p className={"text-BodyText text-textColor text-center font-helvetica font-light"}>
+            <p className={"text-BodyText text-textColor text-center font-helvetica font-normal lg:px-96"}>
             Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna.
           </p>
           </div>
-          <div className="grid grid-cols-3 space-x-2 rounded-md py-16">
-          <div className="rounded-md">
+          <div className="grid grid-cols-3 space-x-2 lg:space-x-10 rounded-md py-16 lg:mx-40">
+          <div className="rounded-md lg:mt-40 ">
           <Image
           src={gallery}
           alt="made in workshop"
           placeholder={"blur"}
           className="rounded-md"/>
           </div>
-          <div className="mt-10 rounded-md">
+          <div className="mt-10 lg:mt-24 rounded-md">
           <Image
           src={gallery}
           alt="made in workshop"
           placeholder={"blur"}
           className="rounded-md"/>
           </div>
-          <div className="mt-20 rounded-md">
+          <div className="mt-20 lg:mt-8 rounded-md">
           <Image
           src={gallery}
           alt="made in workshop"
