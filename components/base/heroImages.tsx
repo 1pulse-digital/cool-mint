d1565a1578adada7721f97759fca8884ed0f820c
Parson <parson@1pulse.co.za>
2024-02-16 17:09:39 +0200

    feat: ✨ updated hero Images component

--- components/base/heroImages.tsx
+++ components/base/heroImages.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import Image from "next/image";
 import gallery from "../../images/banner.webp";
+import Button from "./button";
+import Link from "next/link";
 
 export const HeroImages = () => {
  return (
-  <div className="bg-background px-8">
-    <div className="grid grid-cols-2 space-x-4">
+  <div className="bg-background ">
+    <div className="grid lg:grid-cols-3 grid-cols-2 lg:mx-40 lg:space-x-10">
+    <div className="">
     <div></div>
     <div className={"rounded-md"}>
         <Image
@@ -16,7 +19,7 @@ export const HeroImages = () => {
         />
       </div>
     </div>
-    <div className="grid grid-cols-2 space-x-4 mt-[-80px]">
+    <div className="lg:pt-20">
     <div className="rounded-md">
       <Image
           src={gallery}
@@ -26,6 +29,19 @@ export const HeroImages = () => {
         /></div>
     <div></div>
     </div>
+    <div className={"grid content-center font-helvetica p-5 lg:text-start lg:pt-20"}>
+          <h1 className={"text-headings text-secondary lg:text-start font-helvetica font-bold py-4 text-center"}>Why us<span className="text-primary">?</span></h1>
+          <p className={"text-BodyText text-textColor lg:text-start text-center font-helvetica font-light"}>
+          Made In Workshop (MIW) is a shared workshop and fabrication studio. Not just a workshop with a
+           collection of tools and machines but a community of like-minded fabricators, manufacturers, and designers.
+          </p>
+          <div className="py-8 text-center lg:text-start">
+          <Link href="/about">
+              <Button color="primary">Learn More</Button></Link>
+          </div>
+        </div>
+        </div>
+        <hr className="flex-grow h-[1px] bg-textColor border-0"></hr>
   </div>
     )
 }
