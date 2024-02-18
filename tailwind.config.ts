c4a1ee5da0e057986b9c8ed4661c55c43a0e6a48
Parson <parson@1pulse.co.za>
2024-02-18 16:15:57 +0200

    feat: ✨ added tailwind debug screens

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -47,7 +47,10 @@ const config = {
       },
     },
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [
+    require("tailwindcss-animate"),
+    require('tailwindcss-debug-screens'),
+  ],
 } satisfies Config
 
 export default config
