2a200269a6cee4892e918eb34b18a35c8f8666b0
Parson <parson@1pulse.co.za>
2024-02-19 15:57:35 +0200

    feat: ✨ updated cafe page: desktop

--- app/cafe/page.tsx
+++ app/cafe/page.tsx
@@ -9,10 +9,21 @@ import React from "react";
  const Cafe = () => {
   return(
     <div>
-      <div className={"bg-background py-20 px-8"}>
-        <div className={"grid content-center font-helvetica pb-16"}>
-          <h1 className={"text-headings text-secondary font-helvetica font-bold py-4 text-center"}>MIW Café</h1>
-          <p className={"text-BodyText text-textColor text-center font-helvetica font-light"}>
+      <div className={"bg-background py-20"}>
+      <div className={"grid content-center font-helvetica pb-16 2xl:mx-72 sm:mx-20 md:mx-52 lg:mx-44"}>
+        <div className="text-xs text-secondary  font-helvetica font-light inline-flex justify-center">
+            <Link href="/">
+          <div>
+            Home 
+            <span className="px-1">|</span>  
+          </div>
+          </Link>
+          <Link href="/cafe">
+          <div className="text-primary">Made in Workshop Cafe</div>
+          </Link>
+          </div>
+          <h1 className={"text-headings text-secondary font-helvetica font-bold py-4 text-center"}>MIW Café<span className="text-primary">.</span></h1>
+          <p className={"text-BodyText text-textColor text-center font-helvetica font-light 2xl:px-52 lg:px-32"}>
           Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
           </p>
         </div>
