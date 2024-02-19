2e4ec197ecb2473ed7865aa9af370dda5e20dcbe
Parson <parson@1pulse.co.za>
2024-02-19 15:57:48 +0200

    feat: ✨ updated cafe component

--- components/base/cafeMIW.tsx
+++ components/base/cafeMIW.tsx
@@ -1,6 +1,7 @@
 import * as React from "react"
 import Image from 'next/image'
 import gallery from "../../images/banner.webp"
+import craig from "../../images/craig.webp"
 
 import { Card, CardContent } from "@/components/ui/card"
 import {
@@ -20,15 +21,15 @@ import {
 
 export function CafeMIW() {
   return (
-    <div>
+    <div className="px-40 sm:px-16 grid justify-center items-center md:px-28 xl:mx-20">
     <Tabs defaultValue="machines">
     <div className="pb-16">
-    <Carousel className="mx-6 pt-6">
+    <Carousel className="mx-6 pt-6 2xl:pt-20">
     <CarouselContent className="">
     <CarouselItem>
-      <TabsList className="bg-background space-x-2">
+      <TabsList className="bg-background space-x-2 2xl:space-x-9 sm:space-x-4 md:space-x-6">
         
-        <TabsTrigger value="machines"><div className="pt-8">
+        <TabsTrigger value="machines"><div className="pt-8 2xl:pt-80 sm:pt-32 md:pt-44 xl:pt-72 lg:pt-52">
             <Image
              src={gallery}
              alt="team member"
@@ -37,7 +38,7 @@ export function CafeMIW() {
              width="400"
              height="400"
            /></div></TabsTrigger>
-        <TabsTrigger value="workshops"><div className="pt-8">
+        <TabsTrigger value="workshops"><div className="pt-8 2xl:pt-80 sm:pt-32 md:pt-44 xl:pt-72 lg:pt-52">
             <Image
              src={gallery}
              alt="team member"
@@ -46,7 +47,7 @@ export function CafeMIW() {
              width="400"
              height="400"
            /></div></TabsTrigger>
-        <TabsTrigger value="general"><div className="pt-8">
+        <TabsTrigger value="general"><div className="pt-8 2xl:pt-80 sm:pt-32 md:pt-44 xl:pt-72 lg:pt-52">
             <Image
              src={gallery}
              alt="team member"
@@ -62,9 +63,10 @@ export function CafeMIW() {
       <CarouselNext />
       </Carousel>
       </div>
+      <div className="flex justify-center">
       <TabsContent className="bg-background font-helvetica" value="machines">
         <Card className="bg-background border-0">
-        <div className="bg-[#2c3033] rounded-xl ">
+        <div className=" rounded-xl ">
         <div className="rounded-md"><Image
              src={gallery}
              alt="team member"
@@ -78,7 +80,7 @@ export function CafeMIW() {
         <Card className="bg-background border-0">
         <div className="bg-[#2c3033] rounded-xl">
         <div className="rounded-md"><Image
-             src={gallery}
+             src={craig}
              alt="team member"
              placeholder={"blur"}
              className="rounded-t-lg"
@@ -98,6 +100,7 @@ export function CafeMIW() {
            </div>
         </Card>
       </TabsContent>
+      </div>
     </Tabs>
     </div>
     
