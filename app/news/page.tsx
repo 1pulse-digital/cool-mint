2a45ab6477deb4bbcf0b1dda854f1fbb091b3c7f
Parson <parson@1pulse.co.za>
2024-02-19 15:59:35 +0200

    feat: ✨ updated news page: desktop

--- app/news/page.tsx
+++ app/news/page.tsx
@@ -14,18 +14,30 @@ import {
     TabsList,
     TabsTrigger,
   } from "@/components/ui/tabs"
+import GetInTouch from "@/components/base/getInTouch";
 
 const News = () => {
   return(
       <div className={"bg-background"}>
-        <div className={"grid content-center font-helvetica p-5 sm:p-10 "}>
-          <h1 className={"text-headings text-secondary font-helvetica font-bold py-4 text-center"}>News</h1>
-          <p className={"text-BodyText text-textColor text-center font-helvetica font-light"}>
+        <div className={"grid content-center font-helvetica p-5 sm:p-10 2xl:mx-60 xl:mx-32 lg:mx-10 md:mx-10"}>
+        <div className="text-xs text-secondary  font-helvetica font-light inline-flex justify-center">
+            <Link href="/">
+          <div>
+            Home 
+            <span className="px-1">|</span>  
+          </div>
+          </Link>
+          <Link href="/news">
+          <div className="text-primary">News</div>
+          </Link>
+          </div>
+          <h1 className={"text-headings text-secondary font-helvetica font-bold py-4 text-center"}>News<span className="text-primary">.</span></h1>
+          <p className={"text-BodyText text-textColor text-center font-helvetica font-light 2xl:px-60 lg:pb-16 sm:px-16 lg:px-60"}>
           Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
           </p>
           <div className="py-8 text-center ">
           <div>
-          <p className={"text-[12px] px-4 text-textColor text-start font-helvetica font-normal"}>
+          <p className={"text-[12px] px-4 text-textColor text-center font-helvetica font-normal"}>
         Filter by
           </p>
     <Tabs defaultValue="news">
@@ -33,16 +45,19 @@ const News = () => {
     <Carousel className="mx-3 pt-3">
     <CarouselContent className="">
     <CarouselItem>
-    <TabsList className="grid w-full grid-cols-3 bg-[#27272A] text-secondary">
+      <div className="2xl:px-72 px-8 sm:px-16 lg:px-60 md:px-28">
+    <TabsList className="grid w-full grid-cols-3 bg-[#27272A] px-3 text-secondary ">
         <TabsTrigger value="news" className="">News</TabsTrigger>
         <TabsTrigger value="diy">DIY</TabsTrigger>
         <TabsTrigger value="events">Events</TabsTrigger>
       </TabsList>
+      </div>
       </CarouselItem>
       </CarouselContent>
       </Carousel>
       </div>
-
+<div className=" w-full grid 2xl:grid-cols-3 lg:grid-cols-3 md:grid-cols-2 space-x-1">
+  <div>
       <TabsContent className="bg-background font-helvetica" value="news">
         <Card className="bg-background border-0">
         <BlogWidget
@@ -55,7 +70,9 @@ const News = () => {
       />
     </Card>
       </TabsContent>
+      </div>
 
+      <div>
       <TabsContent className="bg-background font-helvetica" value="news">
     <Card className="bg-background border-0">
         <BlogWidget
@@ -68,20 +85,39 @@ const News = () => {
       /> 
      </Card>
       </TabsContent>
+      </div>
+      <div>
+      <TabsContent className="bg-background font-helvetica" value="news">
+    <Card className="bg-background border-0">
+        <BlogWidget
+        imageUrl="/icons/banner.webp"
+        date="3 January 2024"
+        author="Parson Gapa"
+        title="Made In Workshop Is The Largest DIY Workshop And Makerspace In South Africa"
+        categories={["News", "Workshops"]}
+        linkUrl="/newsTitle"
+      /> 
+     </Card>
+      </TabsContent>
+      </div>
 
+
+      <div>
       <TabsContent className="bg-background font-helvetica" value="events">
         <Card className="bg-background border-0">
        <BlogWidget
         imageUrl="/icons/craig.webp"
         date="3 January 2024"
         author="Parson Gapa"
-        title="Small business showcase: Made in workshop"
+        title="Made in Workshop"
         categories={["News", "Workshops"]}
         linkUrl="/newsTitle"
       />
     </Card>
       </TabsContent>
+      </div>
 
+      <div>
       <TabsContent className="bg-background font-helvetica" value="diy">
       <Card className="bg-background border-0">
         <BlogWidget
@@ -94,17 +130,32 @@ const News = () => {
       />
         </Card>
       </TabsContent>
+      </div>
+      <div>
+      <TabsContent className="bg-background font-helvetica" value="diy">
+      <Card className="bg-background border-0">
+        <BlogWidget
+        imageUrl="/icons/banner.webp"
+        date="Second Date"
+        author="Second Author"
+        title="Made In Workshop Is The Largest DIY Workshop And Makerspace In South Africa"
+        categories={["Workshops", "News"]}
+        linkUrl="/newsTitle"
+      />
+        </Card>
+      </TabsContent>
+      </div>
+      </div>
     </Tabs>
     </div>
           </div>
-          <div className="text-center">
+          <div className="text-center lg:py-20 md:py-10 sm:py-8 py-6">
             <Link href="/">
             <Button color={"primary"}>Learn More</Button>
             </Link>
             </div>
         </div>
-       
-        
+        <GetInTouch />
      </div>
   );
 };
