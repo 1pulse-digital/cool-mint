d1ea3e89096292f78f95f8febc677f754843eb3a
Parson <parson@1pulse.co.za>
2024-02-20 16:06:55 +0200

    feat: ✨ updated machines page

--- app/machines/page.tsx
+++ app/machines/page.tsx
@@ -1,32 +1,42 @@
+import React from "react";
 import IndustrialMachineTools from "@/components/base/IndustrialMachineTools";
 import Button from "@/components/base/button";
-import { FAQSBanner } from "@/components/base/faqsBanner";
-import { FilterButtons } from "@/components/base/filterButtons";
 import GetInTouch from "@/components/base/getInTouch";
 import Link from "next/link";
-import React from "react";
+
 
 const MachinesTools = () => {
   return(
-      <div className={"grid grid-cols-1 bg-background py-12"}>
-        <div className="px-8 ">
+      <div className={" bg-background lg:py-32"}>
+        <div className="">
         <div className={"grid content-center font-helvetica"}>
+          <div className="text-xs text-secondary font-helvetica font-normal inline-flex justify-center items-center">
+            <Link href="/">
+          <div>
+            Home 
+            <span className="px-1">|</span>  
+          </div>
+          </Link>
+          <Link href="/machines">
+           <div className="text-primary">Machines</div>
+          </Link>
+          </div>
+          <div className="2xl:mx-60">
           <h1 className={"text-headings text-secondary font-helvetica font-bold py-4 text-center leading-tight"}>Industrial Tools & Machines</h1>
-          <p className={"text-BodyText text-textColor text-center font-helvetica font-light"}>
+          <p className={"text-BodyText text-textColor text-center font-helvetica font-nromal 2xl:px-60"}>
           Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
           </p>
-          <div className=" text-center pt-10">
-         <FilterButtons />
+         </div>
+          <div className="text-center pt-10">
          <IndustrialMachineTools />
          <div className="pb-16 text-center">
-          <Link href="/" className="">
+          <Link href="/">
             <Button color={"primary"}>Learn More</Button>
           </Link>
         </div>
           </div>
         </div>
         </div>
-        <FAQSBanner />
         <GetInTouch />
      </div>
   );
