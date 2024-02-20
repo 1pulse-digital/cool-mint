0e982eca1e8210c19ad574b7de4c62d6d06fd31b
Parson <parson@1pulse.co.za>
2024-02-20 16:07:39 +0200

    feat: ✨ updated register page

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -4,13 +4,27 @@ import google from "../../images/google.png";
 import Button, { LongButton, SmallButtonOrange } from "@/components/base/button";
 import { InputComponent } from "@/components/base/inputComponents";
 import Link from "next/link";
+import gallery from "../../images/banner.webp";
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
 
 
 const Register: React.FC = () => {
   return (
-    <div className={"bg-background py-12 px-8"}>
+    <div className={"bg-background py-10"}>
+      <div className="grid grid-cols-1 md:grid-cols-2">
+       <div className="hidden md:block">
+        <div>
+          <Image
+            src={gallery}
+            alt="made in workshop"
+            placeholder={"blur"}
+          />
+        </div>
+      </div>
+      <div className="grid 2xl:mx-52 2xl:place-content-end px-8 2xl:px-0 md:px-16">
       <div className={"grid content-center font-helvetica  md:max-w-prose"}>
-          <h1 className={"text-headings text-secondary font-helvetica leading-tight font-bold py-8 text-center"}>Create an account</h1>
+      <h1 className={"text-headings text-secondary font-helvetica font-bold py-2 lg:py-0 md:py-0 text-center sm:text-start md:text-start"}>Create an account</h1>
           </div> 
           <div className="flex-row inline-flex space-x-4">
           <div className="pt-4">
@@ -27,7 +41,12 @@ const Register: React.FC = () => {
     </div>
     </div>
     <div className="py-4">
-    <InputComponent label="Email*" name="email" placeholder="henry@madeinworkshop.co.za" />
+    <div>
+           <Label htmlFor="email" className="text-secondary mt-2">Email</Label>
+           <div className="mt-2">
+      <Input type="email" id="email" placeholder="henry@madeinworkshop.co.za" className="placeholder:text-xs placeholder:px-3 placeholder:font-helvetica placeholder:text-white block w-full rounded-md border-0 border-amber-300 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-[#707070]  focus:ring-2 focus:ring-inset focus:ring-background sm:text-sm sm:leading-6 bg-background"></Input>/
+      </div>
+        </div>
     <div> <Button color="primary" className="w-full mt-4">Register with Email</Button></div>
     <div className="flex items-center justify-center py-10 space-x-4">
     <hr className="flex-grow h-[1px] mx-2 bg-secondary border-0"></hr>
@@ -49,6 +68,8 @@ const Register: React.FC = () => {
         </span>
           </LongButton></div>
           <p className="text-textColor text-center font-helvetica py-10 text-xs">By clicking continue, you agree to our <a href="/" className="underline">Terms of Service</a> and <a href="/" className="underline">Privacy Policy</a>.</p>
+          </div>
+          </div>
           </div>
     </div>
   );
