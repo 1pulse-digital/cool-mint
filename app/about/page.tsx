449cb311f43a86bfc1f86eb42f94a6839bb03464
Parson <parson@1pulse.co.za>
2024-02-20 16:05:14 +0200

    feat: ✨ updated about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -4,11 +4,12 @@ import gallery from "/images/banner.webp";
 import GetInTouch from "@/components/base/getInTouch";
 import Carousel from "@/components/base/Carousel";
 import { TeamCarousel } from "@/components/base/teamCarousel";
+import Link from "next/link";
 
 export const About = () => {
   return (
-    <div className="bg-background">
-      <div className="rounded-md">
+    <div className="bg-background py-20">
+      <div className="hidden ">
         <Image
           src={gallery}
           alt="made in workshop"
@@ -18,38 +19,45 @@ export const About = () => {
       </div>
       <div
         className={
-          "grid content-center p-5 font-helvetica sm:p-10 md:max-w-prose"
+          "grid content-center p-5 font-helvetica "
         }
-      >
+      > <div className="text-xs text-secondary  font-helvetica font-normal inline-flex justify-center">
+      <Link href="/">
+    <div>
+      Home 
+      <span className="px-1">|</span>  
+    </div>
+    </Link>
+    <Link href="/about">
+    <div className="text-primary">About</div>
+    </Link>
+    </div>
+    <div className="lg:mx-80">
         <h1
           className={
             "py-4 text-center font-helvetica text-headings font-bold text-secondary"
           }
         >
-          Where it all began.<span className="text-primary">.</span>
+          Where it all began<span className="text-primary">.</span>
         </h1>
         <p
           className={
-            "text-textColor text-center font-helvetica text-BodyText font-light"
+            "text-textColor text-center font-helvetica text-BodyText font-normal"
           }
         >
-          In 2016 Made In Workshop opened its doors with only a small laser
-          cutter, 3D printers and miscellaneous hand tools and machines. As
-          members came and went, Made In Workshop moulded a space that was not
-          only an essential asset to our customers/members it was also their
-          community.
+          In 2016 Made In Workshop opened its doors with only a small laser cutter, 3D printers 
+          and miscellaneous hand tools and machines. As members came and went, 
+          Made In Workshop moulded a space that was not only an essential asset 
+          to our customers/members it was also their community.
           <br />
           <br />
-          Made In Workshop is not just about the tools and machines. Made In
-          Workshop is not just about the workspace.
-          <br />
-          <br />
-          Made In Workshop is the spark of inspiration that is the culmination
-          of tools, machines, workspace and the community that we are creating.
-          <br />
+          Made In Workshop is <span className="text-primary">not just about the tools</span> and <span className="text-primary">machines </span>and not just about
+           the <span className="text-primary">workspace</span>, we’re the spark of inspiration that is the culmination 
+           of tools, machines, workspace and the community that we are creating.
         </p>
       </div>
-      <h1 className={"text-headings text-secondary font-helvetica font-bold py-16 text-center"}>Meet the Team<span className="text-primary">.</span></h1>
+      <h1 className={"text-headings text-secondary font-helvetica font-bold pt-20 text-center"}>Meet the Team<span className="text-primary">.</span></h1>
+      </div>
       <TeamCarousel />
       <GetInTouch />
     
