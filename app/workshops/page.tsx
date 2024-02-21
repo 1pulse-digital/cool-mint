29e2a985cb8b9d12bc4e0100558abf86ef63784f
Parson <parson@1pulse.co.za>
2024-02-21 11:18:40 +0200

    feat: ✨ updated workshops page

--- app/workshops/page.tsx
+++ app/workshops/page.tsx
@@ -1,25 +1,47 @@
-import { FAQSBanner } from "@/components/base/faqsBanner";
-import { FilterButtons } from "@/components/base/filterButtons";
+import Link from "next/link";
+import React from "react";
 import GetInTouch from "@/components/base/getInTouch";
 import UpcomingWorkshops from "@/components/base/upcomingWorkshops";
-import React from "react";
 
 const Workshops = () => {
-  return(
-      <div className={"grid grid-cols-1 bg-background py-12 "}>
-        <div className={"grid content-center font-helvetica sm:p-10 md:max-w-prose px-8"}>
-          <h1 className={"text-headings text-secondary font-helvetica font-bold py-4 text-center leading-tight"}>Upcoming Workshops</h1>
-          <p className={"text-BodyText text-textColor text-center font-helvetica font-light"}>
-          Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
-          </p>
+  return (
+    <div className={"grid grid-cols-1 bg-background lg:py-32"}>
+      <div className="lg:mx-28">
+        <div className={"grid content-center px-8 font-helvetica"}>
+          <div className="inline-flex justify-center  font-helvetica text-xs font-light text-secondary">
+            <Link href="/">
+              <div>
+                Home
+                <span className="px-1">|</span>
+              </div>
+            </Link>
+            <Link href="/workshops">
+              <div className="text-primary">Workshop</div>
+            </Link>
           </div>
-          <div className=" text-center px-8">
-       
-         </div>
-        <UpcomingWorkshops />
-        <FAQSBanner />
-        <GetInTouch />
-     </div>
+          <h1
+            className={
+              "py-4 text-center font-helvetica text-headings font-bold leading-tight text-secondary"
+            }
+          >
+            Upcoming Workshops <span className="text-primary">.</span>
+          </h1>
+          <p
+            className={
+              "text-center font-helvetica text-BodyText font-light text-textColor lg:px-96"
+            }
+          >
+            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+            nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
+            erat,
+          </p>
+        </div>
+        <div className="">
+          <UpcomingWorkshops />
+        </div>
+      </div>
+      <GetInTouch />
+    </div>
   );
 };
 
