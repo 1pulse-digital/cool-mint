42cdd744710dbb06dc595b9aa37ebedd8d461835
Parson <parson@1pulse.co.za>
2024-02-27 16:00:02 +0200

    feat: ✨ updated about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Image from "next/image";
 import gallery from "/images/banner.webp";
-import GetInTouch from "@/components/base/getInTouch";
 import { TeamCarousel } from "@/components/base/teamCarousel";
 import Link from "next/link";
 
@@ -9,7 +8,47 @@ const About = () => {
   return (
     <div className="bg-background font-helvetica">
       <div className="lg:px-32">
-        <div className="relative -mt-20 flex h-auto flex-col justify-center px-8 py-20 text-center text-primary sm:px-28 sm:py-20 md:h-screen md:px-36 lg:px-28  2xl:px-80">
+
+      {/* <div className="flex space-x-44 ">
+
+<div className="bg-purple-200 absolute"><Image
+              src={gallery}
+              alt="made in workshop"
+              placeholder={"blur"}
+              height={300}
+              width={300}
+            /></div>
+<div className=" bg-yellow-400 w-11/12 justify-center items-center mt-48"> <p
+                className={
+                  "text-center font-helvetica text-BodyText font-normal text-textColor"
+                }
+              >
+                In 2016 Made In Workshop opened its doors with only a small
+                laser cutter, 3D printers and miscellaneous hand tools and
+                machines. As members came and went, Made In Workshop moulded a
+                space that was not only an essential asset to our
+                customers/members it was also their community.
+                <br />
+                <br />
+                Made In Workshop is{" "}
+                <span className="text-primary">
+                  not just about the tools
+                </span>{" "}
+                and <span className="text-primary">machines </span>and not just
+                about the <span className="text-primary">workspace</span>, we&apos;re
+                the spark of inspiration that is the culmination of tools,
+                machines, workspace and the community that we are creating.
+              </p></div>
+<div className="bg-slate-500 relative items-end justify-end grid top-0 left-0 right-0"><Image
+             src={gallery}
+             alt="made in workshop"
+             placeholder={"blur"}
+             height={300}
+             width={300}
+            /></div>
+</div> */}
+
+        <div className="relative -mt-20 flex h-auto flex-col justify-center px-8 py-20 text-center text-primary sm:px-28 sm:py-20 md:h-screen md:px-36 lg:px-28 2xl:px-80">
           <div className="hidden">
             <Image
               src={gallery}
@@ -19,7 +58,6 @@ const About = () => {
             />
           </div>
           <div className={"grid content-center  font-helvetica"}>
-            {" "}
             <div className="inline-flex justify-center  font-helvetica text-xs font-normal text-secondary">
               <Link href="/">
                 <div>
@@ -52,7 +90,7 @@ const About = () => {
                 customers/members it was also their community.
                 <br />
                 <br />
-                Made In Workshop is{" "}
+                Made In Workshop is
                 <span className="text-primary">
                   not just about the tools
                 </span>{" "}
@@ -64,9 +102,9 @@ const About = () => {
             </div>
           </div>
         </div>
-        <div className="">
-          <hr className="h-[1px] flex-grow border-0 bg-[#A1A1AA]"></hr>
-        </div>
+        <div className="px-8">
+            <hr className="h-[1px] flex-grow border-0 bg-[#A1A1AA]"></hr>
+          </div>
         <TeamCarousel />
       </div>
     </div>
