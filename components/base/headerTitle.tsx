33fa0854f2391334ebd2f0737b4be1e5449b1b2b
Parson <parson@1pulse.co.za>
2024-02-27 16:07:28 +0200

    feat: ✨ developed header title component

---
+++ components/base/headerTitle.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+interface HeaderTitleProps {
+  title: string;
+  description: string;
+}
+
+const HeaderTitle: React.FC<HeaderTitleProps> = ({ title, description }) => {
+  return (
+    <div className="2xl:mx-60">
+      <h1 className="text-headings text-secondary font-helvetica font-bold py-4 text-center leading-tight">{title}<span className="text-primary">.</span></h1>
+      <p className="text-BodyText text-textColor text-center font-helvetica font-normal 2xl:px-60">
+        {description}
+      </p>
+    </div>
+  );
+};
+
+export default HeaderTitle;
