49b506d75b58cbe82c402ba5e3baec945b73875f
Parson <parson@1pulse.co.za>
2024-02-27 15:59:05 +0200

    feat: ✨ updated home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,8 @@
 import React from "react";
 import Hero from "@/components/hero";
 import { HeroImages } from "@/components/base/heroImages";
-import { WhyUs } from "@/components/whyUs";
 import { Community } from "@/components/community";
 import UpcomingWorkshops from "@/components/base/upcomingWorkshops";
-import GetInTouch from "@/components/base/getInTouch";
 import Offerings from "@/components/offerings";
 
 export default function Home() {
