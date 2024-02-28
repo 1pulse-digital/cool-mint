4e0f7d2ab8f9e943d7e8a2d3a8a1cf0c215d8a80
Parson <parson@1pulse.co.za>
2024-02-28 15:56:52 +0200

    feat: ✨ updated header component

--- components/base/headerTitle.tsx
+++ components/base/headerTitle.tsx
@@ -7,7 +7,7 @@ interface HeaderTitleProps {
 
 const HeaderTitle: React.FC<HeaderTitleProps> = ({ title, description }) => {
   return (
-    <div className="2xl:mx-60">
+    <div className="2xl:mx-20 xl:mx-80 lg:mx-40 md:mx-16 px-6">
       <h1 className="text-headings text-secondary font-helvetica font-bold py-4 text-center leading-tight">{title}<span className="text-primary">.</span></h1>
       <p className="text-BodyText text-textColor text-center font-helvetica font-normal 2xl:px-60">
         {description}
