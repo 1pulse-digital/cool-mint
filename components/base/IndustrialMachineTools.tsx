817be98f16cbdd078b9cb0d177841f0e5221d35d
Parson <parson@1pulse.co.za>
2024-03-06 12:50:35 +0200

    feat: ✨ updated  responseviness

--- components/base/IndustrialMachineTools.tsx
+++ components/base/IndustrialMachineTools.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import machine1Image from "../../images/CNC-Machining-Center.webp";
 import machine2Image from "../../images/cnc-router.webp";
+import drumsander from "../../images/drumsander.webp";
 import IndustrialMachine from "./industrialMachine";
 import {
   Card,
@@ -30,13 +31,31 @@ const IndustrialMachineTools: React.FC = () => {
 
   const machine2Specs = [
   
-    { label: "Software", value: "CNC Software (UCCNC)" },
-      { label: "Working Area", value: "100mm x 400mm" },
-    { label: "Additional Notes", value: "CNC Software (UCCNC)" },
+    { label: "Software", value: "Feeder table" },
+      { label: "Working Area", value: "1.3 meters wide" },
+    { label: "Additional Notes", value: "twin drum action (currently set to 80 and 120 grit)" },
+  ];
+
+  const CNCPlasmaSpecs = [
+  
+    { label: "Software", value: " USB Stick" },
+      { label: "Working Area", value: "1100mm X 1400mm" },
+    { label: "Additional Notes", value: "Language G & M, 3 Phase Power" },
+  ];
+
+  const CNCRouterSpecs = [
+  
+    { label: "Interface", value: " USB Stick" },
+      { label: "Working Area", value: "1100mm X 1400mm" },
+      { label: "Accuracy", value: "0.02mm" },
+      { label: "Spindal", value: "9Kw, 21000 RPM" },
+    { label: "Additional Notes", value: "Language G & M, 3 Phase Power" },
   ];
 
   const machine1SpecGroups = [{ specs: machine1Specs }];
   const machine2SpecGroups = [{ specs: machine2Specs }];
+  const CNCPlasmaSpecGroups = [{ specs: CNCPlasmaSpecs}];
+  const CNCRouterSpecGroups = [{ specs: CNCRouterSpecs}];
 
   return (
     <div className="bg-background py-8">
@@ -55,11 +74,31 @@ const IndustrialMachineTools: React.FC = () => {
           <CardHeader>
             <CardDescription className="text-textColor">
             <IndustrialMachine
+        machineName="Drum Sander"
+        categories="Category 1 | Category 2"
+        image={drumsander}
+        description=" HCL’s Camworks post processor the Omni is now capable of 2D and 3D machining. 
+        Not just straight forward cutting, but the delicate machining of 3D features such as fillets."
+        specGroups={machine2SpecGroups}
+        buttonUrl={"/about"}
+      />
+            </CardDescription>
+          </CardHeader>
+          <CardContent className="space-y-2">
+          </CardContent>
+        </Card>
+      </TabsContent>
+
+      <TabsContent className="bg-background font-helvetica pt-0" value="woodwork">
+        <Card className="bg-background border-0">
+          <CardHeader>
+            <CardDescription className="text-textColor">
+            <IndustrialMachine
         machineName="CNC Plasma Cutting"
         categories="Category 1 | Category 2"
         image={machine1Image}
-        description="Dummy text is text that is used in the publishing industry or by web designers to occupy the space which will later be filled with 'real' content."
-        specGroups={machine1SpecGroups}
+        description="Suitable for cutting sheet metal from 0.5 to 15mm thick. CNC plasma cutting provides the most economical way of cutting sheet metal in a precise way."
+        specGroups={CNCPlasmaSpecGroups}
         buttonUrl={"/about"}
       />
             </CardDescription>
@@ -74,30 +113,12 @@ const IndustrialMachineTools: React.FC = () => {
           <CardHeader>
             <CardDescription className="text-textColor">
             <IndustrialMachine
-        machineName="Machine Name 2"
+        machineName="CNC Router"
         categories="Category 1 | Category 2"
-        image={machine1Image}
-        description="Dummy text is text that is used in the publishing industry or by web designers to occupy the space which will later be filled with 'real' content."
-        specGroups={machine1SpecGroups}
-        buttonUrl={"/about"}
-      />
-            </CardDescription>
-          </CardHeader>
-          <CardContent className="space-y-2">
-          </CardContent>
-        </Card>
-      </TabsContent>
-
-      <TabsContent className="bg-background font-helvetica pt-0" value="woodwork">
-        <Card className="bg-background border-0">
-          <CardHeader>
-            <CardDescription className="text-textColor">
-            <IndustrialMachine
-        machineName="Machine Name 3"
-        categories="Category 1 | Category 2"
-        image={machine1Image}
-        description="Dummy text is text that is used in the publishing industry or by web designers to occupy the space which will later be filled with 'real' content."
-        specGroups={machine1SpecGroups}
+        image={drumsander}
+        description="The Omni would be just a normal traditional CNC router but with the addition of 
+        HCL’s Camworks post processor the Omni is now capable of 2D and 3D machining."
+        specGroups={CNCRouterSpecGroups}
         buttonUrl={"/about"}
       />
             </CardDescription>
