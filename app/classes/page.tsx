73a921a1efde03f88651a58aac74f1f90a4624ea
Parson <parson@1pulse.co.za>
2024-03-06 12:45:25 +0200

    feat: ✨ added header title component

--- app/classes/page.tsx
+++ app/classes/page.tsx
@@ -2,11 +2,12 @@ import Link from "next/link";
 import React from "react";
 import UpcomingWorkshops from "@/components/base/upcomingWorkshops";
 import GetInTouch from "@/components/base/getInTouch";
+import HeaderTitle from "@/components/base/headerTitle";
 
 const Workshops = () => {
   return (
     <div className={"grid grid-cols-1 bg-background py-20"}>
-      <div className="lg:mx-28">
+      <div className="">
         <div className={"grid content-center px-8 font-helvetica"}>
           <div className="inline-flex justify-center font-helvetica text-xs font-normal text-secondary">
             <Link href="/">
@@ -19,23 +20,17 @@ const Workshops = () => {
               <div className="text-primary">Classes</div>
             </Link>
           </div>
-          <h1
-            className={
-              "py-4 text-center font-helvetica text-headings font-bold leading-tight text-secondary"
-            }
-          >
-            Upcoming Classes<span className="text-primary">.</span>
-          </h1><p
-            className={
-              "text-center font-helvetica text-BodyText font-light text-textColor lg:px-96"
-            }
-          >
-            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
-            nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
-            erat.
-          </p>
+
+          <div>
+       <HeaderTitle title="Upcoming Classes"
+        description="Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+        nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
+        erat."
+      />
+      </div>
+        
         </div>
-        <div>
+        <div className="lg:mx-16">
           <UpcomingWorkshops />
         </div>
       </div>
