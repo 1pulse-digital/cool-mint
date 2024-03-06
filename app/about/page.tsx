527d987e7a5b78566f460ba3facb14b9a6586d70
Parson <parson@1pulse.co.za>
2024-03-06 19:12:05 +0200

    feat: ✨ updated absolute images and text higlight

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -10,47 +10,23 @@ const About = () => {
     <div className="bg-background font-helvetica">
       <div className="lg:px-32">
         <div className="relative hidden lg:flex">
-        <Image
-              src={gallery}
-              alt="made in workshop"
-              placeholder={"blur"}
-              height={250}
-              width={250}
-              className="absolute top-8 left-0 -rotate-12 lg:w-40 xl:w-60 rounded-lg 2xl:mt-16"
-            />
-            <Image
-              src={gallery}
-              alt="made in workshop"
-              placeholder={"blur"}
-              height={250}
-              width={250}
-              className="absolute mt-[430px] lg:mt-[460px] xl:mt-[380px] 2xl:mt-[480px] right-0 rotate-12 lg:w-40 xl:w-60 rounded-lg"
-            />
+          <Image
+            src={gallery}
+            alt="made in workshop"
+            placeholder={"blur"}
+            height={250}
+            width={250}
+            className="absolute left-0 top-8 -rotate-12 rounded-lg lg:w-40 lg:pt-20 xl:w-60 2xl:mt-16"
+          />
+          <Image
+            src={gallery}
+            alt="made in workshop"
+            placeholder={"blur"}
+            height={250}
+            width={250}
+            className="absolute right-0 mt-[430px] rotate-12 rounded-lg lg:mt-[660px] lg:w-40 xl:mt-[450px] xl:w-60 2xl:mt-[480px]"
+          />
         </div>
-       
-{/* <div className=" bg-yellow-400 w-1/2 justify-center items-center mt-48 relative"> <p
-                className={
-                  "text-center font-helvetica text-BodyText font-normal text-textColor"
-                }
-              >
-                In 2016 Made In Workshop opened its doors with only a small
-                laser cutter, 3D printers and miscellaneous hand tools and
-                machines. As members came and went, Made In Workshop moulded a
-                space that was not only an essential asset to our
-                customers/members it was also their community.
-                <br />
-                <br />
-                Made In Workshop is{" "}
-                <span className="text-primary">
-                  not just about the tools
-                </span>{" "}
-                and <span className="text-primary">machines </span>and not just
-                about the <span className="text-primary">workspace</span>, we&apos;re
-                the spark of inspiration that is the culmination of tools,
-                machines, workspace and the community that we are creating.
-              </p></div> */}
-
-
         <div className="relative -mt-20 flex h-auto flex-col justify-center px-8 py-20 text-center text-primary sm:px-28 sm:py-20 md:h-screen md:px-36 lg:px-28 xl:px-80 2xl:px-80">
           <div className="hidden">
             <Image
@@ -93,19 +69,20 @@ const About = () => {
                 customers/members it was also their community.
                 <br />
                 <br />
-                Made In Workshop is<span className="text-primary"> not just about the tools
-                </span>{" "}
-                and <span className="text-primary">machines </span>and not just
-                about the <span className="text-primary">workspace</span>, we&apos;re
-                the spark of inspiration that is the culmination of tools,
-                machines, workspace and the community that we are creating.
+                Made In Workshop is not just about the tools and machines and
+                not just about the workspace,{" "}
+                <span className="text-primary">
+                  we&apos;re the spark of inspiration that is the culmination of
+                  tools, machines, workspace and the community that we are
+                  creating.
+                </span>
               </p>
             </div>
           </div>
         </div>
         <div className="px-8">
-            <hr className="h-[1px] flex-grow border-0 bg-[#A1A1AA]"></hr>
-          </div>
+          <hr className="h-[1px] flex-grow border-0 bg-[#A1A1AA]"></hr>
+        </div>
         <TeamCarousel />
       </div>
       <div className="py-20 lg:px-8 2xl:px-24">
