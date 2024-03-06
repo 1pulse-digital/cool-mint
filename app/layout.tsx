d5da141e53a0ebff5eef882af991bf24939cdf79
Parson <parson@1pulse.co.za>
2024-03-06 19:11:16 +0200

    feat: ✨ removed space

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,7 +21,6 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${isDev ? "debug-screens " : ""}${inter.className}`}>
-       
         <Header />
         {children}
         <Footer />
