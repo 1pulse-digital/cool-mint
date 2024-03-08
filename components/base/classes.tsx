c00981b1fc060e3c991548f7ec4b34c9a1e9d375
Parson <parson@1pulse.co.za>
2024-03-08 16:13:47 +0200

    feat: ✨ updated responsivesness of images

--- components/base/classes.tsx
+++ components/base/classes.tsx
@@ -93,7 +93,7 @@ export function Classes() {
         </div>
       <Tabs defaultValue="class1">
         <div className="grid justify-center items-center sm:mx-32">
-          <Carousel className="mx-10 pt-10 2xl:mx-80 xl:mx-72 lg:mx-32 md:mx-28">
+          <Carousel className="mx-10 sm:pt-10 2xl:mx-96 xl:mx-72 lg:mx-32 md:mx-28">
             <CarouselContent>
               <CarouselItem>
                 <TabsList className="bg-background">
@@ -148,87 +148,7 @@ export function Classes() {
             <CarouselNext />
           </Carousel>
         </div>
-       {/*  <TabsContent className="bg-background font-helvetica grid justify-center" value="class1">
-          <Card className="border-0 bg-background">
-            <div className={"mx-8 grid content-center py-8 font-helvetica mx-80 justify-center"}>
-            <h1
-                className={
-                  "text-start sm:text-center font-helvetica text-headings font-semibold text-textColor"
-                }
-              >
-              Workshops Name<span className="text-primary">.</span>
-              </h1>
-              <div className="row flex space-x-4 justify-center items-center">
-                <span className="inline-flex items-center py-1 text-xs text-secondary">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    strokeWidth="1.5"
-                    stroke="#FDBB10"
-                    className="mr-2 h-4 w-4"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="M12 6v6h4.5m4.5 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
-                    />
-                  </svg>
-                  3 hours
-                </span>
-                <span className="inline-flex py-1 text-xs text-secondary">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    strokeWidth="1.5"
-                    stroke="#FDBB10"
-                    className="mr-2 h-4 w-4"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z"
-                    />
-                  </svg>
-                  Henry Levine
-                </span>
-              </div>
-              <span className="text-md pt-8 font-helvetica font-bold text-primary">
-                R1,500.00
-              </span>
-              <p
-                className={
-                  "text-center font-helvetica text-[12px] font-normal text-secondary"
-                }
-              >
-                Max 6 pax per session
-              </p>
-              <p
-                className={
-                  "pt-8 text-center font-helvetica text-BodyText text-sm font-normal text-secondary"
-                }
-              >
-                Dummy text is text that is used in the publishing industry or by
-                web designers to occupy the space which will later be filled
-                with real content. This is required when, for example, the final
-                text is not yet available. Dummy text is also known as fill
-                text. It is said that song composers of the past used dummy
-                texts as lyrics when writing melodies in order to have a
-                ready-made text to sing with the melody. Dummy texts have been
-                in use by typesetters since the 16th century.
-              </p>
-              <p
-                className={
-                  "pt-6 text-center font-helvetica text-[12px] font-normal text-secondary"
-                }
-              >
-                *Please note that this class is not a tradesman qualification.
-              </p>
-            </div>
-          </Card>
-        </TabsContent> */}
-
+    
         <TabsContent className="bg-background font-helvetica" value="class2">
           <Card className="border-0 bg-background">
          
