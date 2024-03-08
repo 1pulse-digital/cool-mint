2dcf7c0232ed0af5d73a382f6d73d2f1cf41eb3b
Parson <parson@1pulse.co.za>
2024-03-08 16:09:42 +0200

    feat: ✨ updated header spacing and borders

--- app/memberships/page.tsx
+++ app/memberships/page.tsx
@@ -9,7 +9,7 @@ const Memberships = () => {
     <div className={"bg-background py-10"}>
       <div
         className={
-          "grid content-center px-8 font-helvetica sm:p-10 "
+          "grid content-center px-8 font-helvetica sm:pt-10"
         }
       >
         <div className="inline-flex justify-center font-helvetica text-xs font-normal text-secondary">
@@ -25,14 +25,11 @@ const Memberships = () => {
         </div>
        <div>
        <HeaderTitle title="Memberships"
-        description="We have membership options that cover all types of customers ranging from professionals to hobbyists . Our Membership model was designed to work in perfect synchronisation with your business cycle. Days do not have to be used in consecutive order and can be used by multiple users on the same day."
+        description="We have membership options that cover all types of customers ranging from professionals to hobbyists . Our Membership model was designed to work in perfect synchronisation with your business cycle. Days do not have to be used in consecutive order and can be used by multiple users on the same day. Prices include VAT."
       />
       </div>
       </div>
-      <div className="pb-20 pt-8 sm:pt-0">
-        <p className="text-center font-helvetica text-BodyText font-normal text-primary">
-          Prices include VAT.
-        </p>
+      <div className="pb-10">
         <PricingTable />
       </div>
       <div className="py-20 lg:px-8 2xl:px-24">
