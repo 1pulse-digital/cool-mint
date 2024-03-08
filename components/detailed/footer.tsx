64525a196eda6b1ee5b643bc5290c7eb6ba5d061
Parson <parson@1pulse.co.za>
2024-03-08 16:12:41 +0200

    feat: ✨ added linkedin social icon

--- components/detailed/footer.tsx
+++ components/detailed/footer.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import Button from "../base/button";
 import Link from "next/link";
 //import ScrollToTop from "react-scroll-to-top";
-import GetInTouch from "../base/getInTouch";
+
 
 export const Footer = () => {
   return (
@@ -81,7 +81,7 @@ export const Footer = () => {
             </div>
           </div>
           <div>
-            <div className="">
+            <div>
               <p className="pb-2 font-helvetica text-BodyText font-bold text-textColor">
                 Visit Us
               </p>
@@ -191,6 +191,21 @@ export const Footer = () => {
                         d="m10 15l5.19-3L10 9zm11.56-7.83c.13.47.22 1.1.28 1.9c.07.8.1 1.49.1 2.09L22 12c0 2.19-.16 3.8-.44 4.83c-.25.9-.83 1.48-1.73 1.73c-.47.13-1.33.22-2.65.28c-1.3.07-2.49.1-3.59.1L12 19c-4.19 0-6.8-.16-7.83-.44c-.9-.25-1.48-.83-1.73-1.73c-.13-.47-.22-1.1-.28-1.9c-.07-.8-.1-1.49-.1-2.09L2 12c0-2.19.16-3.8.44-4.83c.25-.9.83-1.48 1.73-1.73c.47-.13 1.33-.22 2.65-.28c1.3-.07 2.49-.1 3.59-.1L12 5c4.19 0 6.8.16 7.83.44c.9.25 1.48.83 1.73 1.73"
                       />
                     </svg>
+                  </Link>
+                </div>
+                <div>
+                  <Link
+                    href="https://za.linkedin.com/company/made-in-workshop"
+                    target="_blank"
+                  >
+                     <svg
+        xmlns="http://www.w3.org/2000/svg"
+        className="h-4 w-5"
+        fill="#FDBB11"
+        viewBox="0 0 24 24"
+      >
+        <path d="M4.98 3.5c0 1.381-1.11 2.5-2.48 2.5s-2.48-1.119-2.48-2.5c0-1.38 1.11-2.5 2.48-2.5s2.48 1.12 2.48 2.5zm.02 4.5h-5v16h5v-16zm7.982 0h-4.968v16h4.969v-8.399c0-4.67 6.029-5.052 6.029 0v8.399h4.988v-10.131c0-7.88-8.922-7.593-11.018-3.714v-2.155z" />
+      </svg>
                   </Link>
                 </div>
               </div>
