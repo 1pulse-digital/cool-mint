0dc1cd4dfa923a32fb470d99636c0d454764e3ce
Parson <parson@1pulse.co.za>
2024-03-11 09:28:57 +0200

    feat: ✨ updated cart buttons and responsiveness

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -1,4 +1,4 @@
-import Button, { SmallButtonOrange } from "@/components/base/button";
+import Button, { LongButton, SmallButtonOrange } from "@/components/base/button";
 import Link from "next/link";
 import ShoppingCart from "@/components/base/shoppingCart";
 import { Label } from "@/components/ui/label";
@@ -9,7 +9,8 @@ import HeaderTitle from "@/components/base/headerTitle";
 const Cart: React.FC = () => {
   return (
     <div className={"bg-background py-20"}>
-      <div className="px-8 xl:mx-28 2xl:mx-36">
+      <div className={"bg-background px-8"}>
+      <div className="xl:mx-28 2xl:mx-36">
         <div className="grid content-center font-helvetica ">
           <div className="inline-flex items-center justify-center font-helvetica text-xs font-normal text-secondary">
             <Link href="/">
@@ -31,26 +32,26 @@ const Cart: React.FC = () => {
         <div>
           <ShoppingCart />
         </div>
-        <div className="">
-          <div className="grid md:flex md:space-x-24">
-            <div className="flex place-items-center space-x-2 sm:space-x-24">
-              <div className="pb-2">
+        <div>
+          <div className="grid md:flex md:space-x-32">
+            <div className="flex place-items-center space-x-2 sm:space-x-10 2xl:space-x-1">
+              <div className="bg-grey-500 flex space-x-6">
+               <div className="py-3">
                 <Label htmlFor="discount" className="mt-2 text-secondary">
                   Add Discount Code:
                 </Label>
-                <div className="mt-2 w-52">
+                <div className="sm:w-60">
                   <Input
                     type="surname"
                     id="surname"
-                    placeholder="disc@20"
-                    className="block w-full rounded-md border-0 border-amber-300 bg-background py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-[#707070] placeholder:px-3 placeholder:font-helvetica placeholder:text-xs  placeholder:text-white focus:ring-2 focus:ring-inset focus:ring-background sm:text-sm sm:leading-6"
+                    placeholder="discount@2024"
+                    className="block rounded-md border-0 border-amber-300 bg-background py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-[#707070] placeholder:px-3 placeholder:font-helvetica placeholder:text-xs placeholder:text-white focus:ring-2 focus:ring-inset focus:ring-background sm:text-sm sm:leading-6"
                   ></Input>
                 </div>
               </div>
-              <div className="flex justify-center pt-4 font-bold">
-                <div>
+              <div className="pt-8">
                 <Link href="/register">
-                  <SmallButtonOrange color="primary">
+                  <SmallButtonOrange color="primary" className="font-bold">
                     Apply
                     <svg
                       className="ms-2 h-3.5 w-3.5 rtl:rotate-180"
@@ -71,13 +72,16 @@ const Cart: React.FC = () => {
                 </Link>
               </div>
               </div>
+              <div className="flex justify-center pt-4 font-bold">
+                
+              </div>
             </div>
             <div>
               <div className="py-2 text-start">
                 <div>
                   <span
                     className={
-                      "text-start font-helvetica text-BodyText text-textColor"
+                      "text-start font-helvetica text-BodyText text-secondary"
                     }
                   >
                     Total:
@@ -93,7 +97,7 @@ const Cart: React.FC = () => {
                   </span>
                 </div>
               </div>
-              <div className="pb-4 text-[16px] text-primary sm:pb-0">
+              <div className="pb-4 text-[16px]  text-textColor sm:pb-0">
                 <div>
                   <span className={"font-helvetica font-bold line-through"}>
                     R1,700.00
@@ -103,15 +107,15 @@ const Cart: React.FC = () => {
             </div>
           </div>
         </div>
-        <div className="items-start space-x-6 sm:flex">
-          <div className="lg:w-full">
+        <div className="sm:space-x-6 sm:flex">
+          <div>
             <Link href="/login">
-              <Button color="primary" className=" mt-4">
+              <LongButton color="secondary" className="mt-4 w-60">
                 Proceed to Checkout
-              </Button>
+              </LongButton>
             </Link>
           </div>
-          <div className="flex items-start justify-start py-2 sm:items-center sm:justify-center">
+          <div className="items-start justify-start py-2 sm:items-center sm:justify-center">
             <Link href="/workshops">
               <SmallButtonOrange
                 color="primary"
@@ -137,6 +141,7 @@ const Cart: React.FC = () => {
             </Link>
           </div>
         </div>
+      </div>
       </div>
       <div className="py-8 sm:py-20 lg:px-8 2xl:px-24">
         <GetInTouch />
