290417d894626c7efb07d571d1225635208d3c77
Parson <parson@1pulse.co.za>
2024-03-13 15:01:11 +0200

    feat: ✨ created samole t and c page

---
+++ app/termsConditions/page.tsx
@@ -0,0 +1,351 @@
+import React from "react";
+import GetInTouch from "@/components/base/getInTouch";
+import Link from "next/link";
+import HeaderTitle from "@/components/base/headerTitle";
+
+const TermsConditions = () => {
+  return (
+    <div className="bg-background py-20">
+      <div className="px-8 font-helvetica sm:px-10 md:px-10 lg:px-10 xl:px-12 2xl:px-28">
+        <div className="inline-flex items-start justify-start  pb-2 font-helvetica text-xs font-normal text-secondary">
+          <Link href="/">
+            <div>
+              Home
+              <span className="px-1">|</span>
+            </div>
+          </Link>
+          <Link href="/">
+            <div className="text-primary">Terms and Conditions</div>
+          </Link>
+        </div>
+        <p className="font-helvetical pb-8 text-4xl font-bold text-secondary">
+          Terms and Conditions
+        </p>
+        <p className="text-base font-normal text-secondary ">
+          We have created this privacy statement for online ecommerce in order
+          to demonstrate our firm and continuing commitment to the privacy of
+          personal information provided by those visiting our website. We hold
+          the privacy of your personal information in the highest regard. The
+          following discloses our information gathering and dissemination
+          practices for this website. We recognize the importance of protecting
+          your privacy and our policy is designed to assist you in understanding
+          how we collect, use and safeguard the personal information you provide
+          to us and to assist you in making informed decisions when using our
+          site. This policy will be continuously assessed against new
+          technologies, business practices and our customers’ needs.
+          <br />
+          <br />
+          What Information Do We Collect?
+          <br />
+          <br />
+          When you visit this website you may provide us with two types of
+          information: personal information you knowingly choose to disclose
+          that is collected on an individual basis and website use information
+          collected on an aggregate basis as you and others browse our Website.
+          <br />
+          <br />
+          Personal Information You Choose to Provide Email Information
+          <br />
+          <br />
+          If you choose to correspond with us through email, we may retain the
+          content of your email messages together with your email address and
+          our responses. We provide the same protections for these electronic
+          communications that we employ in the maintenance of information
+          received by mail and telephone.
+          <br />
+          <br />
+          Website Use Information Similar to other commercial websites, our
+          website utilizes a standard technology called “cookies” (see
+          explanation below, “What Are Cookies?”) and web server log files to
+          collect information about how our website is used. Information
+          gathered through cookies and Web server logs may include the date and
+          time of visits, the pages viewed, time spent at our Website, and the
+          Websites visited just before and just after our Website.
+          <br />
+          <br />
+          How Do We Use the Information That You Provide to Us?
+          <br />
+          <br />
+          Broadly speaking, we use personal information for purposes of
+          administering our business activities, providing the products and
+          services you requested, to process your payment, to monitor the use of
+          the service, our marketing and promotional efforts and improve our
+          content and service offerings, and customize our site’s content,
+          layout, services and for other lawful purposes. These uses improve our
+          site and better tailor it to meet your needs.
+          <br />
+          <br />
+          Furthermore, such information may be shared with others on an
+          aggregate basis. Personally identifiable information or business
+          information will not be shared with parties except as required by law.
+          <br />
+          <br />
+          Occasionally, we may also use the information we collect to notify you
+          about important changes to our Website, new services, and special
+          offers we think you will find valuable. You may notify us at any time
+          if you do not wish to receive these offers by emailing us at the link
+          provided on the newsletter.
+          <br />
+          <br />
+          What Are Cookies?
+          <br />
+          <br />
+          A cookie is a very small text document, which often includes an
+          anonymous unique identifier. When you visit a website, that site’s
+          computer asks your computer for permission to store this file in a
+          part of your hard drive specifically designated for cookies. Each
+          Website can send its own cookie to your browser if your browser’s
+          preferences allow it, but (to protect your privacy) your browser only
+          permits a Website to access the cookies it has already sent to you,
+          not the cookies sent to you by other sites. Browsers are usually set
+          to accept cookies. However, if you would prefer not to receive
+          cookies, you may alter the configuration of your browser to refuse
+          cookies. If you choose to have your browser refuse cookies, it is
+          possible that some areas of our site will not function as effectively
+          when viewed by the users. A cookie cannot retrieve any other data from
+          your hard drive or pass on computer viruses.
+          <br />
+          <br />
+          How Do We Use Information We Collect from Cookies?
+          <br />
+          <br />
+          As you visit and browse our Website, the site uses cookies to
+          differentiate you from other users. In some cases, we also use cookies
+          to prevent you from having to log in more than is necessary for
+          security. Cookies, in conjunction with our Web server’s log files,
+          allow us to calculate the aggregate number of people visiting our
+          Website and which parts of the site are most popular. This helps us
+          gather feedback to constantly improve our Website and better serve our
+          clients. Cookies do not allow us to gather any personal information
+          about you and we do not intentionally store any personal information
+          that your browser provided to us in your cookies.
+          <br />
+          <br />
+          Who we are
+          <br />
+          <br />
+          Our website address is: https://madeinworkshop.co.za/
+          <br />
+          <br />
+          What personal data we collect and why we collect it.
+          <br />
+          <br />
+          Comments
+          <br />
+          <br />
+          When visitors leave comments on the site we collect the data shown in
+          the comments form, and also the visitor’s IP address and browser user
+          agent string to help spam detection. An anonymized string created from
+          your email address (also called a hash) may be provided to the
+          Gravatar service to see if you are using it. The Gravatar service
+          privacy policy is available here: https://automattic.com/privacy/.
+          After approval of your comment, your profile picture is visible to the
+          public in the context of your comment.
+          <br />
+          <br />
+          How long we retain your data
+          <br />
+          <br />
+          If you leave a comment, the comment and its metadata are retained
+          indefinitely. This is so we can recognize and approve any follow-up
+          comments automatically instead of holding them in a moderation queue.
+          <br />
+          <br />
+          For users that register on our website (if any), we also store the
+          personal information they provide in their user profile. All users can
+          see, edit, or delete their personal information at any time (except
+          they cannot change their username). Website administrators can also
+          see and edit that information.
+          <br />
+          <br />
+          What rights you have over your data
+          <br />
+          <br />
+          If you have an account on this site, or have left comments, you can
+          request to receive an exported file of the personal data we hold about
+          you, including any data you have provided to us. You can also request
+          that we erase any personal data we hold about you. This does not
+          include any data we are obliged to keep for administrative, legal, or
+          security purposes.
+          <br />
+          <br />
+          Where we send your data
+          <br />
+          <br />
+          Visitor comments may be checked through an automated spam detection
+          service.
+          <br />
+          <br />
+          DoubleClick DART cookies
+          <br />
+          <br />
+          We also may use DART cookies for ad serving through Google’s
+          DoubleClick, which places a cookie on your computer when you are
+          browsing the web and visits a site using DoubleClick advertising
+          (including some Google AdSense advertisements). This cookie is used to
+          serve ads specific to you and your interests (”interest based
+          targeting”). The ads served will be targeted based on your previous
+          browsing history (For example, if you have been viewing sites about
+          visiting Las Vegas, you may see Las Vegas hotel advertisements when
+          viewing a non-related site, such as on a site about hockey). DART uses
+          “non personally identifiable information”. It does NOT track personal
+          information about you, such as your name, email address, physical
+          address, telephone number, social security numbers, bank account
+          numbers or credit card numbers. You can opt-out of this ad serving on
+          all sites using this advertising by visiting
+          http://www.doubleclick.com
+          <br />
+          <br />
+          You can choose to disable or selectively turn off our cookies or
+          third-party cookies in your browser settings, or by managing
+          preferences in programs such as Norton Internet Security. However,
+          this can affect how you are able to interact with our site as well as
+          other websites. This could include the inability to login to services
+          or programs, such as logging into forums or accounts.
+          <br />
+          <br />
+          Deleting cookies does not mean you are permanently opted out of any
+          advertising program. Unless you have settings that disallow cookies,
+          the next time you visit a site running the advertisements, a new
+          cookie will be added.
+          <br />
+          <br />
+          IP Addresses
+          <br />
+          <br />
+          IP addresses are used by your computer every time you are connected to
+          the Internet. Your IP address is a number that is used by computers on
+          the network to identify your computer. IP addresses are automatically
+          collected by our web server as part of demographic and profile data
+          known as traffic data so that data (such as the Web pages you request)
+          can be sent to you.
+          <br />
+          <br />
+          Sharing and Selling Information
+          <br />
+          <br />
+          We do not share, sell, lend or lease any of the information that
+          uniquely identifies a subscriber (such as email addresses or personal
+          details) with anyone except to the extent it is necessary to process
+          transactions or provide services that you have requested.
+          <br />
+          <br />
+          How Can You Access and Correct Your Information?
+          <br />
+          <br />
+          You may request access to all your personally identifiable information
+          that we collect online and maintain in our database by emailing us at
+          the usual address.
+          <br />
+          <br />
+          What About Legally Compelled Disclosure of Information?
+          <br />
+          <br />
+          We may disclose information when legally compelled to do so, in other
+          words, when we, in good faith, believe that the law requires it or for
+          the protection of our legal rights. We may also disclose account
+          information when we have reason to believe that disclosing this
+          information is necessary to identify, contact or bring legal action
+          against someone who may be violating our Terms of Service or to
+          protect the safety of our users and the Public.
+          <br />
+          <br />
+          What About Other WebSites Linked to Our WebSite?
+          <br />
+          <br />
+          We are not responsible for the practices employed by websites linked
+          to or from our website or the information or content contained
+          therein. Often links to other Websites are provided solely as pointers
+          to information on topics that may be useful to the users of our
+          Website.
+          <br />
+          <br />
+          Please remember that when you use a link to go from our Website to
+          another website, our Privacy Policy is no longer in effect. Your
+          browsing and interaction on any other website, including websites,
+          which have a link on our Website, is subject to that website’s own
+          rules and policies. Please read over those rules and policies before
+          proceeding.
+          <br />
+          <br />
+          Your Consent
+          <br />
+          <br />
+          By using our website you consent to our collection and use of your
+          personal information as described in this Privacy Policy. We reserve
+          the right to amend this privacy policy at any time with or without
+          notice.
+          <br />
+          <br />
+          Our Commitment To Data Security:
+          <br />
+          <br />
+          Please note that your information will be stored and processed on our
+          computers in the United States. The laws on holding personal data in
+          the United States may be less stringent than the laws of your Country
+          of residence or citizenship. To prevent unauthorized access, maintain
+          data accuracy, and ensure the correct use of information, we have put
+          in place appropriate physical, electronic, and managerial procedures
+          to safeguard and secure the information we collect online.
+          <br />
+          <br />
+          Choice/Opt-In/Opt-Out
+          <br />
+          <br />
+          This site allows visitors to unsubscribe so that they will not receive
+          future messages. After unsubscribing we will discontinue sending the
+          particular messages as soon as technically feasible.
+          <br />
+          <br />
+          Surveys & Contests
+          <br />
+          <br />
+          From time-to-time our site requests information from users via surveys
+          or contests. Participation in these surveys or contests is completely
+          voluntary and the user therefore has a choice whether or not to
+          disclose this information. Information requested may include contact
+          information (such as name and shipping address), and demographic
+          information (such as zip code, age level). Contact information will be
+          used to notify the winners and award prizes. Survey information will
+          be used for purposes of monitoring or improving the use and
+          satisfaction of this site.
+          <br />
+          <br />
+          A Special Note About Children
+          <br />
+          <br />
+          Children are not eligible to use our services unsupervised and we ask
+          that children (under the age of 14) do not submit any personal
+          information to us. If you are a minor, you can use this service only
+          in conjunction with permission and guidance from your parents or
+          guardians.
+          <br />
+          <br />
+          Acquisition or Changes in Ownership
+          <br />
+          <br />
+          In the event that the website (or a substantial portion of its assets)
+          is acquired, your information would be considered part of those
+          assets, and may be part of those assets that are transferred.
+          <br />
+          <br />
+          Policy Modifications
+          <br />
+          <br />
+          We may change this Privacy Policy from time to time. If/when changes
+          are made to this privacy policy, we will email users who have given us
+          permission to do so. We will post any changes here, so be sure to
+          check back periodically. However, please be assured that if the
+          Privacy Policy changes in the future, we will not use the personal
+          information you have submitted to us under this Privacy Policy in a
+          manner that is materially inconsistent with this Privacy Policy,
+          without your prior consent.
+        </p>
+      </div>
+      <div className="py-20 lg:px-8 2xl:px-24">
+        <GetInTouch />
+      </div>
+    </div>
+  );
+};
+export default TermsConditions;
