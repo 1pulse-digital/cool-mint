1310a1dd3667c25c43106d7c3e67218a9c144a43
Parson <parson@1pulse.co.za>
2024-04-09 15:53:49 +0200

    feat: ✨ copy-updates

--- components/community.tsx
+++ components/community.tsx
@@ -6,11 +6,14 @@ export const Community = () => {
   return (
     <div className="flex h-screen flex-col items-center justify-center p-5 font-helvetica">
       <h1 className="py-4 text-center text-headings font-bold leading-tight text-secondary">
-        Maker Community<span className="text-primary">.</span>
+        Makerspace Community<span className="text-primary">.</span>
       </h1>
       <span className="text-center  text-BodyText font-light text-textColor xl:px-40 2xl:px-52">
-        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy
-        eirmod tempor invidunt ut labore et dolore magna.
+        The makerspace community is more than a trend; it's a movement towards
+        innovation, entrepreneurship and education. Made in Workshop is proud to
+        be at the forefront of this movement, fostering a culture of creativity
+        and collaboration that contributes significantly to the local economy
+        and community.
       </span>
       <div className="mt-10 flex justify-center rounded-md  ">
         <Image
