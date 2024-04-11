e92043b331a6a055b99885e21ee52e8dd54c5f0d
Erick <erick@meshtrade.co>
2024-04-11 02:21:03 +0200

    Add remotePatterns to next.config.js for image optimization

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import("next").NextConfig} */
 const nextConfig = {
-  output: "standalone"
+  output: "standalone",
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "storage.googleapis.com",
+        pathname: "/made-in-workshop/**",
+      },
+    ],
+  },
 };
 
 module.exports = nextConfig;
