edda92539db2446eb40664785468f4d35f36b3d5
Parson <parson@1pulse.co.za>
2024-04-13 15:19:33 +0200

    feat: ✨ cleaned-code-mapping image-items

--- components/base/cafeMIW.tsx
+++ components/base/cafeMIW.tsx
@@ -1,12 +1,11 @@
 import * as React from "react";
-import Image from "next/image";
+import Image, { StaticImageData } from "next/image";
 import sandwich from "../../images/cafe/Cheese-&-Tomato-Sandwich.jpg";
 import chips from "../../images/cafe/Chips-and-Sandwich.jpg";
 import coffee from "../../images/cafe/Cups-of-Coffee.jpg";
 import bacon from "../../images/cafe/Egg,-Bacon-and-Cheese-Sandwich.jpg";
 import cheese from "../../images/cafe/Ham-and-Cheese-Sandwich.jpg";
 import plateofchips from "../../images/cafe/Plate-of-Chips.jpg";
-
 
 import { Card, CardContent } from "@/components/ui/card";
 import {
@@ -18,7 +17,51 @@ import {
 } from "@/components/ui/carousel";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
+interface CafeItem {
+  name: string;
+  alt: string;
+  src: StaticImageData;
+}
+
 export function CafeMIW() {
+  const items: CafeItem[] = [
+    {
+      name: "sandwich",
+      alt: "Cheese-&-Tomato-Sandwich",
+      src: sandwich,
+    },
+
+    {
+      name: "chips",
+      alt: "Chips-and-Sandwich",
+      src: chips,
+    },
+
+    {
+      name: "coffee",
+      alt: "Cups-of-Coffee",
+      src: coffee,
+    },
+
+    {
+      name: "bacon",
+      alt: "Egg,-Bacon-and-Cheese-Sandwich",
+      src: bacon,
+    },
+
+    {
+      name: "cheese",
+      alt: "Ham-and-Cheese-Sandwich",
+      src: cheese,
+    },
+
+    {
+      name: "plateofchips",
+      alt: "Plate-of-Chips",
+      src: plateofchips,
+    },
+  ];
+
   return (
     <div className="grid items-center justify-center sm:px-16 md:px-20 xl:mx-20">
       <Tabs defaultValue="sandwich">
@@ -27,77 +70,37 @@ export function CafeMIW() {
             <CarouselContent className="">
               <CarouselItem>
                 <TabsList className="space-x-2 bg-background sm:space-x-4 md:space-x-6 2xl:space-x-9">
-                  <TabsTrigger value="sandwich">
-                    <div className="pt-28 sm:pt-32 md:pt-44 lg:pt-52 xl:pt-72 2xl:pt-80">
-                      <Image
-                        src={sandwich}
-                        alt="Cheese-&-Tomato-Sandwich"
-                        className="rounded-lg"
-                        width="400"
-                        height="400"
-                      />
-                    </div>
-                  </TabsTrigger>
-                  <TabsTrigger value="chips">
-                    <div className="pt-28 sm:pt-32 md:pt-44 lg:pt-52 xl:pt-72 2xl:pt-80">
-                      <Image
-                        src={chips}
-                        alt="Chips-and-Sandwich"
-                        className="rounded-lg"
-                        width="400"
-                        height="400"
-                      />
-                    </div>
-                  </TabsTrigger>
-                  <TabsTrigger value="coffee">
-                    <div className="pt-28 sm:pt-32 md:pt-44 lg:pt-52 xl:pt-72 2xl:pt-80">
-                      <Image
-                        src={coffee}
-                        alt="Cups-of-Coffee"
-                        className="rounded-lg"
-                        width="400"
-                        height="400"
-                      />
-                    </div>
-                  </TabsTrigger>
+                  {items.slice(0, 3).map((item, index) => (
+                    <TabsTrigger key={index} value={item.name}>
+                      <div className="pt-28 sm:pt-32 md:pt-44 lg:pt-52 xl:pt-72 2xl:pt-80">
+                        <Image
+                          src={item.src}
+                          alt={item.alt}
+                          className="rounded-lg"
+                          width="400"
+                          height="400"
+                        />
+                      </div>
+                    </TabsTrigger>
+                  ))}
                 </TabsList>
               </CarouselItem>
 
               <CarouselItem>
                 <TabsList className="space-x-2 bg-background sm:space-x-4 md:space-x-6 2xl:space-x-9">
-                  <TabsTrigger value="bacon">
-                    <div className="pt-28 sm:pt-32 md:pt-44 lg:pt-52 xl:pt-72 2xl:pt-80">
-                      <Image
-                        src={bacon}
-                        alt="Egg,-Bacon-and-Cheese-Sandwich"
-                        className="rounded-lg"
-                        width="400"
-                        height="400"
-                      />
-                    </div>
-                  </TabsTrigger>
-                  <TabsTrigger value="cheese">
-                    <div className="pt-28 sm:pt-32 md:pt-44 lg:pt-52 xl:pt-72 2xl:pt-80">
-                      <Image
-                        src={cheese}
-                        alt="Ham-and-Cheese-Sandwich"
-                        className="rounded-lg"
-                        width="400"
-                        height="400"
-                      />
-                    </div>
-                  </TabsTrigger>
-                  <TabsTrigger value="plateofchips">
-                    <div className="pt-28 sm:pt-32 md:pt-44 lg:pt-52 xl:pt-72 2xl:pt-80">
-                      <Image
-                        src={plateofchips}
-                        alt="Plate-of-Chips"
-                        className="rounded-lg"
-                        width="400"
-                        height="400"
-                      />
-                    </div>
-                  </TabsTrigger>
+                  {items.slice(-3).map((item, index) => (
+                    <TabsTrigger key={index} value={item.name}>
+                      <div className="pt-28 sm:pt-32 md:pt-44 lg:pt-52 xl:pt-72 2xl:pt-80">
+                        <Image
+                          src={item.src}
+                          alt={item.alt}
+                          className="rounded-lg"
+                          width="400"
+                          height="400"
+                        />
+                      </div>
+                    </TabsTrigger>
+                  ))}
                 </TabsList>
               </CarouselItem>
             </CarouselContent>
@@ -106,93 +109,25 @@ export function CafeMIW() {
           </Carousel>
         </div>
         <div className="flex justify-center">
-          <TabsContent
-            className="bg-background font-helvetica"
-            value="sandwich"
-          >
-            <Card className="border-0 bg-background">
-              <div className=" rounded-xl ">
-                <div className="rounded-md">
-                  <Image
-                    src={sandwich}
-                    alt="Chips-and-Sandwich"
-                    className="rounded-lg"
-                  />
+          {items.map((item, index) => (
+            <TabsContent
+              className="bg-background font-helvetica"
+              value={item.name}
+              key={index}
+            >
+              <Card className="border-0 bg-background">
+                <div className="rounded-xl bg-[#2c3033]">
+                  <div className="rounded-md">
+                    <Image
+                      src={item.src}
+                      alt={item.alt}
+                      className="rounded-lg"
+                    />
+                  </div>
                 </div>
-              </div>
-            </Card>
-          </TabsContent>
-          <TabsContent
-            className="bg-background font-helvetica"
-            value="chips"
-          >
-            <Card className="border-0 bg-background">
-              <div className="rounded-xl bg-[#2c3033]">
-                <div className="rounded-lg">
-                  <Image
-                    src={chips}
-                    alt="Chips-and-Sandwich"
-                    className="rounded-lg"
-                  />
-                </div>
-              </div>
-            </Card>
-          </TabsContent>
-          <TabsContent className="bg-background font-helvetica" value="coffee">
-            <Card className="border-0 bg-background">
-              <div className="rounded-xl bg-[#2c3033]">
-                <div className="rounded-md">
-                  <Image
-                    src={coffee}
-                    alt="Cups-of-Coffee"
-                    className="rounded-lg"
-                  />
-                </div>
-              </div>
-            </Card>
-          </TabsContent>
-
-          <TabsContent className="bg-background font-helvetica" value="cheese">
-            <Card className="border-0 bg-background">
-              <div className="rounded-xl bg-[#2c3033]">
-                <div className="rounded-md">
-                  <Image
-                    src={cheese}
-                    alt="Ham-and-Cheese-Sandwich"
-                    className="rounded-lg"
-                  />
-                </div>
-              </div>
-            </Card>
-          </TabsContent>
-
-          <TabsContent className="bg-background font-helvetica" value="bacon">
-            <Card className="border-0 bg-background">
-              <div className="rounded-xl bg-[#2c3033]">
-                <div className="rounded-md">
-                  <Image
-                    src={bacon}
-                    alt="Egg,-Bacon-and-Cheese-Sandwich"
-                    className="rounded-lg"
-                  />
-                </div>
-              </div>
-            </Card>
-          </TabsContent>
-
-          <TabsContent className="bg-background font-helvetica" value="plateofchips">
-            <Card className="border-0 bg-background">
-              <div className="rounded-xl bg-[#2c3033]">
-                <div className="rounded-md">
-                  <Image
-                    src={plateofchips}
-                    alt="Plate-of-Chips"
-                    className="rounded-lg"
-                  />
-                </div>
-              </div>
-            </Card>
-          </TabsContent>
+              </Card>
+            </TabsContent>
+          ))}
         </div>
       </Tabs>
     </div>
