a056ace22d08665e649540bc2fe336bafa5eb241
Parson <parson@1pulse.co.za>
2024-04-23 14:16:31 +0200

    feat: ✨ responsive-updates

--- components/base/industrialMachine.tsx
+++ components/base/industrialMachine.tsx
@@ -1,19 +1,19 @@
-import { Machine } from "@/lib/fusion/workshop/machine.pb";
-import Image from "next/image";
-import React from "react";
-import { AspectRatio } from "../ui/aspect-ratio";
+import { Machine } from "@/lib/fusion/workshop/machine.pb"
+import Image from "next/image"
+import React from "react"
+import { AspectRatio } from "../ui/aspect-ratio"
 
 interface Spec {
-  label: string;
-  value: string;
+  label: string
+  value: string
 }
 
 interface SpecGroup {
-  specs: Spec[];
+  specs: Spec[]
 }
 
 interface IndustrialMachineProps {
-  machine: Machine;
+  machine: Machine
 }
 
 const IndustrialMachine: React.FC<IndustrialMachineProps> = ({ machine }) => {
@@ -22,17 +22,19 @@ const IndustrialMachine: React.FC<IndustrialMachineProps> = ({ machine }) => {
       <div>
         <div className="rounded-lg bg-[#2c3033] lg:flex 2xl:mx-40">
           <div className="w-full lg:w-3/5">
-          <AspectRatio ratio={4 / 3}>
-            <Image
-              src={machine.gallery.thumbnail.url}
-              alt={machine.gallery.thumbnail.description || machine.displayName}
-              fill
-              sizes="(max-width: 768px) 100vw, (max-width: 1280px) 50vw, 33vw"
-              className="rounded-t-lg object-cover sm:rounded-t-lg lg:rounded-l-lg"
-            />
-          </AspectRatio>
+            <AspectRatio ratio={4 / 3}>
+              <Image
+                src={machine.gallery.thumbnail.url}
+                alt={
+                  machine.gallery.thumbnail.description || machine.displayName
+                }
+                fill
+                sizes="(max-width: 768px) 100vw, (max-width: 1280px) 50vw, 33vw"
+                className="rounded-t-lg object-cover sm:rounded-t-lg lg:rounded-l-lg"
+              />
+            </AspectRatio>
           </div>
-          <div className="rounded-lg bg-[#2c3033] p-5 py-8 sm:px-8 w-2/3 ">
+          <div className="w-full rounded-lg bg-[#2c3033] p-5 py-8 sm:px-8 lg:w-2/3">
             <div className={"grid text-start font-helvetica"}>
               <h3
                 className={
@@ -41,12 +43,12 @@ const IndustrialMachine: React.FC<IndustrialMachineProps> = ({ machine }) => {
               >
                 {machine.displayName}
               </h3>
-              <p className="py-2 pb-6 font-helvetica sm:text-Body text-sm text-primary">
+              <p className="py-2 pb-6 font-helvetica text-sm text-primary sm:text-Body">
                 {machine.categories.join(" | ")}
               </p>
               <p
                 className={
-                  "text-start font-helvetica sm:text-Body text-sm font-normal text-textColor"
+                  "text-start font-helvetica text-sm font-normal text-textColor sm:text-Body"
                 }
               >
                 {machine.description}
@@ -60,10 +62,10 @@ const IndustrialMachine: React.FC<IndustrialMachineProps> = ({ machine }) => {
               Specs
             </h3>
             <div className="space-x-10">
-              <div className="grid grid-cols-3 pb-4 md:pb-0 text-start font-helvetica">
+              <div className="grid grid-cols-2 pb-4 text-start font-helvetica md:space-x-10 md:pb-0 lg:space-x-1">
                 {machine.attributes.map((attribute, idx) => (
                   <div key={idx}>
-                    <p className="font-helvetica sm:text-Body text-sm text-primary">
+                    <p className="font-helvetica text-sm text-primary sm:text-Body">
                       {" "}
                       {attribute.name}:
                     </p>
@@ -79,7 +81,7 @@ const IndustrialMachine: React.FC<IndustrialMachineProps> = ({ machine }) => {
         </div>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default IndustrialMachine;
+export default IndustrialMachine
