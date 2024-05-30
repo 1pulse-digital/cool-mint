da9b2fe3a9fec09f393368213d814c2dc61cc946
Erick <erick@meshtrade.co>
2024-05-30 19:16:17 +0200

    fix: fix the 404 page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,28 +1,38 @@
-import { SmallButtonOrange } from "@/components/base/button";
-import Link from "next/link";
-import React from "react";
+import { Button } from "@/components/ui/button"
+import Link from "next/link"
 
 function notFound() {
-    return (
-        <div className={"bg-background py-12 px-8 "}>
-        <div className={"self-center content-center font-helvetica sm:p-10 md:max-w-prose"}>
-        <span className={"text-BodyText text-primary text-center font-helvetica font-light"}>
-         404
-          </span>
-          <h1 className={"text-headings text-foreground font-helvetica font-bold py-4 text-center"}>Page not found</h1>
-          <span className={"text-BodyText text-muted-foreground text-center font-helvetica font-light"}>
-          Sorry, we couldn&apos;t find the page you&apos;re looking for.
-          </span>
-          <div className="py-8 text-center ">
-          <Link href="/">
-          <SmallButtonOrange color="primary">Continue browsing
-        <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-<path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9"/></svg>
-</SmallButtonOrange></Link>
-          </div>
-        </div>
-     </div>
-    )
+  return (
+    <div className={"flex flex-col h-full justify-center"}>
+      <span className={"text-center text-xl font-light text-primary"}>404</span>
+      <h1 className={"py-4 text-center text-4xl font-bold"}>Page not found</h1>
+      <span className={"text-center text-xl font-light text-muted-foreground"}>
+        Sorry, we couldn&apos;t find the page you&apos;re looking for.
+      </span>
+      <div className="py-8 text-center ">
+        <Link href="/">
+          <Button variant={"link"} color="primary">
+            Continue browsing
+            <svg
+              className="ms-2 h-3.5 w-3.5 rtl:rotate-180"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 14 10"
+            >
+              <path
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M1 5h12m0 0L9 1m4 4L9 9"
+              />
+            </svg>
+          </Button>
+        </Link>
+      </div>
+    </div>
+  )
 }
 
-export default notFound;
+export default notFound
