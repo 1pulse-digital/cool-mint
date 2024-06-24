01bc96b9d52f5bdf746fc3f3fd2f61440e769279
Erick <erick@meshtrade.co>
2024-06-24 22:33:12 +0200

    refactor: improve the header-title

---
+++ components/header-title.tsx
@@ -0,0 +1,22 @@
+import React from "react"
+
+interface HeaderTitleProps {
+  children: React.ReactNode
+  description?: string
+}
+
+const HeaderTitle: React.FC<HeaderTitleProps> = ({ children, description }) => {
+  return (
+    <div className="px-6 md:mx-16 lg:mx-40 xl:mx-60 2xl:mx-40">
+      <h1 className="py-4 text-center font-helvetica text-headings font-bold leading-tight text-foreground">
+        {children}
+        <span className="text-primary">.</span>
+      </h1>
+      <p className="text-center font-helvetica text-BodyText font-normal text-muted-foreground 2xl:px-60">
+        {description}
+      </p>
+    </div>
+  )
+}
+
+export default HeaderTitle
