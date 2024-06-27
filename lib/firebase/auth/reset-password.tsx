14ff0822a5d496d5881f059b6b989312b8fd97a8
Erick <erick@meshtrade.co>
2024-06-27 18:58:22 +0200

    chore: cleanup and don't toast

--- lib/firebase/auth/reset-password.tsx
+++ lib/firebase/auth/reset-password.tsx
@@ -3,19 +3,21 @@
 import firebase_app from "../config"
 
 import { AuthError, getAuth, sendPasswordResetEmail } from "firebase/auth"
-import { toast } from "sonner"
+
+const auth = getAuth(firebase_app)
 
 interface ResetPasswordResult {
-    error: AuthError | Error | null
-    }
+  error: AuthError | Error | null
+}
 
-export default async function resetPassword(email: string): Promise<ResetPasswordResult> {
-    let error: AuthError | Error | null = null
-    try {
-        await sendPasswordResetEmail(getAuth(firebase_app), email)
-        toast.success("Password reset email sent.")
-    } catch (e) {
-        error = e as AuthError | Error
-    }
-    return { error }
+export default async function resetPassword(
+  email: string,
+): Promise<ResetPasswordResult> {
+  let error: AuthError | Error | null = null
+  try {
+    await sendPasswordResetEmail(getAuth(firebase_app), email)
+  } catch (e) {
+    error = e as AuthError | Error
+  }
+  return { error }
 }
