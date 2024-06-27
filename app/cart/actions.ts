de0d2dfaba0462efabe0167a433078f556c4d789
Erick <erick@meshtrade.co>
2024-06-27 18:50:40 +0200

    fix: error message

--- app/cart/actions.ts
+++ app/cart/actions.ts
@@ -3,7 +3,12 @@ import { initTransport } from "@/lib/transport"
 import { TwirpError } from "twirpscript"
 
 import { authHeader } from "@/app/actions"
-import { MyCart, MyCartRequest, RemoveFromCart, RemoveFromCartRequest } from "@/lib/fusion/commerce/cart.manager.pb"
+import {
+  MyCart,
+  MyCartRequest,
+  RemoveFromCart,
+  RemoveFromCartRequest,
+} from "@/lib/fusion/commerce/cart.manager.pb"
 import { Cart } from "@/lib/fusion/commerce/cart.pb"
 
 initTransport()
@@ -15,23 +20,23 @@ export async function myCart(request: MyCartRequest): Promise<Cart> {
     })
   } catch (e: unknown) {
     if (e instanceof TwirpError) {
-      console.error(`cart`, e)
-      throw new Error(`cart: ${e.msg}`)
+      throw new Error(e.msg)
     }
-    throw e
+    throw new Error("Failed to get cart")
   }
 }
 
-export async function removeFromCart(request: RemoveFromCartRequest): Promise<Cart> {
+export async function removeFromCart(
+  request: RemoveFromCartRequest,
+): Promise<Cart> {
   try {
     return await RemoveFromCart(request, {
       headers: await authHeader(),
     })
   } catch (e: unknown) {
     if (e instanceof TwirpError) {
-      console.error(`remove from cart`, e)
-      throw new Error(`remove from cart: ${e.msg}`)
+      throw new Error(e.msg)
     }
-    throw e
+    throw new Error("Failed to remove from cart")
   }
 }
