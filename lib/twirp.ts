f3e8a3265a72cc264aca8c0e3b3dbb89f4f217a6
Erick <erick@meshtrade.co>
2024-06-27 22:25:19 +0200

    chore: comment out unused code

--- lib/twirp.ts
+++ lib/twirp.ts
@@ -1,9 +1,9 @@
 import { ClientConfiguration } from "twirpscript"
 
-export const config: ClientConfiguration = {
-  baseURL: "https://fusion-cva6vopela-bq.a.run.app",
-  prefix: "/api/v1",
-  headers: {
-    Authorization: "Bearer token",
-  },
-}
+// export const config: ClientConfiguration = {
+//   baseURL: "https://fusion-cva6vopela-bq.a.run.app",
+//   prefix: "/api/v1",
+//   headers: {
+//     Authorization: "Bearer token",
+//   },
+// }
