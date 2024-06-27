01dbc72cb18866f3df02a168e48a417e9912a04f
Erick <erick@meshtrade.co>
2024-06-27 18:56:59 +0200

    fix: user context and auth redirects

--- contexts/user.tsx
+++ contexts/user.tsx
@@ -1,24 +1,41 @@
 "use client"
+
+import { handleLogin, handleLogout } from "@/app/actions"
 import { onAuthStateChanged } from "@/lib/firebase/auth/state-changed"
 import { User } from "firebase/auth"
-// import { useRouter } from "next/navigation"
+import { useRouter, useSearchParams } from "next/navigation"
 import { createContext, useContext, useEffect, useState } from "react"
+import { toast } from "sonner"
 
 const UserContext = createContext({} as User | null)
 
 export const useUser = () => useContext(UserContext)
 
 export const UserProvider = ({ children }: { children: React.ReactNode }) => {
+  const [hasLoggedIn, setHasLoggedIn] = useState(false)
   const [user, setUser] = useState<User | null>(null)
-  // const router = useRouter()
+  const router = useRouter()
+  const searchParams = useSearchParams()
+  const redirect = searchParams.get("redirect") ?? ""
 
   useEffect(() => {
-    const unsubscribe = onAuthStateChanged((user) => {
+    const unsubscribe = onAuthStateChanged(async (user) => {
       if (user) {
+        const tokenResult = await user.getIdTokenResult()
+        await handleLogin(tokenResult)
+        setHasLoggedIn(true)
         setUser(user)
+        if (!hasLoggedIn) {
+          toast.success("Signed in.")
+          router.push("/" + redirect)
+        }
       } else {
-        // router.push("/login")
+        // don't take the user to /login automatically
+        await handleLogout()
         setUser(null)
+        if (hasLoggedIn) {
+          toast.success("Signed out.")
+        }
       }
     })
     return () => unsubscribe()
