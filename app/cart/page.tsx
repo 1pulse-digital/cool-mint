a48ee9d28bbd845a4a2066e8bf37deae54d69986
Erick <erick@meshtrade.co>
2024-06-27 18:50:56 +0200

    chore: remove unused code

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -7,26 +7,7 @@ import { myCart } from "./actions"
 import { ShoppingCart } from "./components/cart"
 import HeaderTitle from "@/components/header-title"
 
-const RightArrow = () => (
-  <svg
-    className="ms-2 h-3.5 w-3.5 rtl:rotate-180"
-    aria-hidden="true"
-    xmlns="http://www.w3.org/2000/svg"
-    fill="none"
-    viewBox="0 0 14 10"
-  >
-    <path
-      stroke="currentColor"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      strokeWidth="2"
-      d="M1 5h12m0 0L9 1m4 4L9 9"
-    />
-  </svg>
-)
-
 const Cart: React.FC = async () => {
-  // TODO: redirect to login if not logged in (DONE by middleware.ts?)
   const cart = await myCart({})
 
   return (
