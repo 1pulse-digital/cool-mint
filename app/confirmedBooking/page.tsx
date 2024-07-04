e8502381561d1ecf26df3c1939e5a496ec1ccde7
Devan McGeer <devan@meshtrade.co>
2024-07-04 07:24:07 +0200

    feat(bookTour): Added breadcrumb and header component to confimed booking

--- app/confirmedBooking/page.tsx
+++ app/confirmedBooking/page.tsx
@@ -5,31 +5,31 @@ import Button, {
 } from "@/components/base/button";
 import Link from "next/link";
 import { useSearchParams } from "next/navigation";
+import { addMinutes, format } from "date-fns";
+import { Breadcrumbs } from "@/components/breadcrumbs";
+import HeaderTitle from "@/components/header-title";
 
 const ConfirmedBooking: React.FC = () => {
   const searchParams = useSearchParams()
-  const tourDay = searchParams.get("tourDay")
-  const startTime = searchParams.get("startTime")
+  const bookingTime = new Date(searchParams.get("time") || "")
 
+  const bookingDay = format(bookingTime, "EEEE, MMMM d, yyyy")
+  const startTime = format(bookingTime, "HH:mm")
+  const endTime = format(addMinutes(bookingTime, 30), "HH:mm")
+  
   // TODO: Think about an add to calendar button
+  // TODO: Refactor and add component
   // -> npm install add-to-calendar-button
   return (
     <div className={"bg-background py-20"}>
       <div className="grid content-center items-center justify-center px-8 font-helvetica">
-        <div className="inline-flex items-center justify-center font-helvetica text-xs font-normal text-foreground">
-          <Link href="/">
-            <div>
-              Home
-              <span className="px-1">|</span>
-            </div>
-          </Link>
-          <Link href="/confirmedBooking">
-            <div className="text-primary">Booking Confirmed</div>
-          </Link>
-        </div>
-        <h1 className="py-8 text-center font-helvetica text-headings font-bold leading-tight text-foreground">
-          Booking Confirmed
-        </h1>
+        <Breadcrumbs
+          crumbs={[
+            { name: "Home", href: "/" },
+            { name: "Booking Confirmed", href: "/confirmedBooking" },
+          ]}
+        />
+        <HeaderTitle>Booking Confirmed</HeaderTitle>
         <div className="flex items-center justify-center py-4 text-[#ADFA1C]">
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -46,92 +46,92 @@ const ConfirmedBooking: React.FC = () => {
             />
           </svg>
         </div>
-   <div className="grid content-center items-center place-items-center">
-        <div className="">
-          <div className="flex flex-row space-x-4 py-1 pt-4 text-center text-primary justify-center">
-            <div>
-              <span
-                className={"text-start font-helvetica text-[16px] font-bold"}
-              >
-                Date:
-              </span>
-            </div>
-            <div>
-              <span
-                className={
-                  "text-start font-helvetica text-[16px] font-bold text-foreground"
-                }
-              >
-               {tourDay}
-              </span>
-            </div>
-          </div>
-          <div className="flex flex-row  py-1 text-[16px] text-primary justify-center">
-            <div className="flex space-x-4">
+        <div className="grid content-center items-center place-items-center">
+          <div className="">
+            <div className="flex flex-row space-x-4 py-1 pt-4 text-center text-primary justify-center">
               <div>
-                <span className={"font-helvetica font-bold"}>Time:</span>
+                <span
+                  className={"text-start font-helvetica text-[16px] font-bold"}
+                >
+                  Date:
+                </span>
               </div>
-              <div className="">
-                <span className={"font-helvetica font-bold text-foreground"}>
-                  {startTime}
+              <div>
+                <span
+                  className={
+                    "text-start font-helvetica text-[16px] font-bold text-foreground"
+                  }
+                >
+                  {bookingDay}
                 </span>
               </div>
             </div>
-          </div>
-          <div className="flex  py-1  pt-4  text-primary justify-center space-x-4">
-            <div>
-              <span
-                className={" font-helvetica text-[16px] font-bold"}
-              >
-                Location:
-              </span>
+            <div className="flex flex-row  py-1 text-[16px] text-primary justify-center">
+              <div className="flex space-x-4">
+                <div>
+                  <span className={"font-helvetica font-bold"}>Time:</span>
+                </div>
+                <div className="">
+                  <span className={"font-helvetica font-bold text-foreground"}>
+                    {startTime} - {endTime}
+                  </span>
+                </div>
+              </div>
             </div>
-            <div className="flex justify-center font-bold text-foreground flex-wrap">
-              <Link
-                href="https://maps.app.goo.gl/j4SW9cavsiZCNjMT6"
-                target="_blank"
-              >
-                <div className="flex flex-wrap items-center text-[16px]">
-                  10 Naaf Street, Strydompark, Randburg, Johanneburg 2169
+            <div className="flex  py-1  pt-4  text-primary justify-center space-x-4">
+              <div>
+                <span
+                  className={" font-helvetica text-[16px] font-bold"}
+                >
+                  Location:
+                </span>
+              </div>
+              <div className="flex justify-center font-bold text-foreground flex-wrap">
+                <Link
+                  href="https://maps.app.goo.gl/j4SW9cavsiZCNjMT6"
+                  target="_blank"
+                >
+                  <div className="flex flex-wrap items-center text-[16px]">
+                    10 Naaf Street, Strydompark, Randburg, Johanneburg 2169
+                  </div>
+                </Link>
+              </div>
+            </div>
+          </div>
+          <div className="pt-10">
+            <div className="mb-20 items-start justify-start py-4 text-center sm:text-start md:flex md:space-x-8 lg:justify-start lg:px-0">
+              <Link href="/">
+                <div className="">
+
+                  <Button color="primary" className="w-80 ">
+                    Add to calendar
+                  </Button>
                 </div>
               </Link>
-            </div>
-          </div>
-        </div>
-        <div className="pt-10">
-          <div className="mb-20 items-start justify-start py-4 text-center sm:text-start md:flex md:space-x-8 lg:justify-start lg:px-0">
-            <Link href="/">
-              <div className="">
-                
-                <Button color="primary" className="w-80 ">
-                  Add to calendar
-                </Button>
+              <div className="flex items-center justify-center">
+                <Link href="/">
+                  <SmallButtonOrange color="primary">
+                    Continue browsing
+                    <svg
+                      className="ms-2 h-3.5 w-3.5 rtl:rotate-180"
+                      aria-hidden="true"
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 14 10"
+                    >
+                      <path
+                        stroke="currentColor"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        d="M1 5h12m0 0L9 1m4 4L9 9"
+                      />
+                    </svg>
+                  </SmallButtonOrange>
+                </Link>
               </div>
-            </Link>
-            <div className="flex items-center justify-center">
-              <Link href="/">
-                <SmallButtonOrange color="primary">
-                  Continue browsing
-                  <svg
-                    className="ms-2 h-3.5 w-3.5 rtl:rotate-180"
-                    aria-hidden="true"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 14 10"
-                  >
-                    <path
-                      stroke="currentColor"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="M1 5h12m0 0L9 1m4 4L9 9"
-                    />
-                  </svg>
-                </SmallButtonOrange>
-              </Link>
             </div>
           </div>
-        </div>
         </div>
       </div>
     </div>
