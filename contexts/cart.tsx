45489de58cd334f010a92d228e2fd3dc708b6861
Devan McGeer <devan@meshtrade.co>
2024-07-18 08:38:33 +0200

    feat(cart): Added wip cart context

---
+++ contexts/cart.tsx
@@ -0,0 +1,12 @@
+import { Cart } from "@/lib/fusion/commerce/cart.pb";
+import { createContext, useContext } from "react";
+
+
+
+const CartContext = createContext({} as Cart | null)
+
+export const useCart = () => useContext(CartContext)
+
+export const CartProvider = ({ children }: { children: React.ReactNode }) => {
+    return <CartContext.Provider value={null}>{children}</CartContext.Provider>
+}
