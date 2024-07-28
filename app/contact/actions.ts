7ca2ac30bf806a39cd7b912a71bee677bc4f2462
Devan McGeer <devan@meshtrade.co>
2024-07-28 19:27:52 +0200

    feat(Contact): :sparkles: Added send email definitions, added test emails, added html styling

--- app/contact/actions.ts
+++ app/contact/actions.ts
@@ -1,8 +1,20 @@
 "use server"
 import { initTransport } from "@/lib/transport"
+import { TwirpError } from "twirpscript"
+import { authHeader } from "../actions"
+import { SendEmail, SendEmailRequest } from "@/lib/fusion/workshop/email.service.pb"
 
 initTransport()
 
-export async function sendContactEmail(){
-    console.log("sendContactEmail")
+export async function sendContactEmail(request: SendEmailRequest) {
+    try {
+        return await SendEmail(request, {
+            headers: await authHeader(),
+        })
+    } catch (e: unknown) {
+        if (e instanceof TwirpError) {
+            console.error("sendContactEmail", e)
+            throw new Error(`sendContactEmail: ${e.code}: ${e.msg}`)
+        }
+    }
 }
