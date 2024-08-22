8abfe30a08768605562ae8aef3a7931ec27cb190
Daniella van der Linde <daniella@1pulse.co.za>
2024-08-22 19:18:56 +0200

    fix: Switch MS Clarity to 1Pulse account

--- app/layout.tsx
+++ app/layout.tsx
@@ -54,7 +54,7 @@ export default function RootLayout({
         c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
         t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
         y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-    })(window, document, "clarity", "script", "nr9qcfsggl");
+    })(window, document, "clarity", "script", "nrddrvg4jl");
   `}
       </Script>
       <GoogleAnalytics gaId="G-CKHPE5XL4R" />
