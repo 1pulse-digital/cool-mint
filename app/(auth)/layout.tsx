b9e3110dbde302d2215af11a92780d7d3c12a5d2
Erick <erick@meshtrade.co>
2024-09-26 18:10:06 +0200

    fix: Add a login/register page image

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,9 +1,18 @@
 import React from "react"
+import Image from "next/image"
+import miw from "./miw-1.png"
 
 const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   return (
     <div className="grid w-full lg:grid-cols-2">
-      <div className="hidden bg-muted lg:block" />
+      <div className="relative hidden h-full w-full bg-muted lg:block">
+        <Image
+          src={miw}
+          alt="Made in Workshop building"
+          layout="fill"
+          objectFit="cover"
+        />
+      </div>
       {children}
     </div>
   )
