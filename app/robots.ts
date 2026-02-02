4cbf7d5799b4ccd0bc817d786c77d738c390264f
Gerhard Otto <gerrieotto123@gmail.com>
2026-02-02 08:12:48 +0200

    fix: update robots rules structure

--- app/robots.ts
+++ app/robots.ts
@@ -2,9 +2,15 @@ import type { MetadataRoute } from 'next'
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-      userAgent: '*',
-      disallow: '/',
-    },
+    rules: [
+      {
+        userAgent: 'Googlebot',
+        allow: '/',
+      },
+      {
+        userAgent: '*',
+        disallow: '/',
+      },
+    ],
   }
 }
